5a9490979ad1cdd548df4814a19513bb7bfa2062
Elemar Rodrigues Severo Junior <elemarjr@windowslive.com>
2014-11-06 15:46:20 -0800

    add argumentexceptiontests

---
+++ test/CodeCracker.Test/ArgumentExceptionTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using TestHelper;
+using CodeCracker;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class ArgumentExceptionTests : CodeFixVerifier
+    {
+        private const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo(int a, int b)
+            {
+                throw new ArgumentException(""message"", ""c"");
+            }
+        }
+    }";
+
+        [Fact]
+        public void WhenThrowingArgumentExceptionWithInvalidArgumentAnalyzerCreatesDiagnostic()
+        {
+            var expected = new DiagnosticResult
+            {
+                Id = ArgumentExceptionAnalyzer.DiagnosticId,
+                Message = "Type argument 'c' is not in the argument list.",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 10, 56)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [Fact]
+        public void WhenThrowingArgumentExceptionWithInvalidArgumentAndApplyingFirstFixUsesFirstParameter()
+        {
+            var fixtest = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo(int a, int b)
+            {
+                throw new ArgumentException(""message"", ""a"");
+            }
+        }
+    }";
+            VerifyCSharpFix(test, fixtest, 0);
+        }
+
+        [Fact]
+        public void WhenThrowingArgumentExceptionWithInvalidArgumentAndApplyingSecondFixUsesSecondParameter()
+        {
+            var fixtest = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo(int a, int b)
+            {
+                throw new ArgumentException(""message"", ""b"");
+            }
+        }
+    }";
+            VerifyCSharpFix(test, fixtest, 1);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new ArgumentExceptionCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ArgumentExceptionAnalyzer();
+        }
+    }
+}
