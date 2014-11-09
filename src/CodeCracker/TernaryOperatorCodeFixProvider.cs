a914c58a4e55599d9b34b4de4ecea0863a9705a9
Giovanni Bassi <giggio@giggio.net>
2014-11-09 08:45:13 -0800

    code cleanup

--- src/CodeCracker/TernaryOperatorCodeFixProvider.cs
+++ src/CodeCracker/TernaryOperatorCodeFixProvider.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
 using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CodeFixes;
-using Microsoft.CodeAnalysis.CodeActions;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.Rename;
-using Microsoft.CodeAnalysis.Text;
-using Microsoft.CodeAnalysis.Formatting;
-using Microsoft.CodeAnalysis.Simplification;
 
 namespace CodeCracker
 {
-    [ExportCodeFixProvider("CodeCrackerTernaryOperatorCodeFixProviderCodeFixProvider", LanguageNames.CSharp), Shared]
+    [ExportCodeFixProvider("CodeCrackerTernaryOperatorCodeFixProvider", LanguageNames.CSharp), Shared]
     public class TernaryOperatorCodeFixProvider : CodeFixProvider
     {
         public sealed override ImmutableArray<string> GetFixableDiagnosticIds()
@@ -41,7 +36,6 @@ namespace CodeCracker
 
         private async Task<Document> MakeTernaryAsync(Document document, IfStatementSyntax ifStatement, CancellationToken cancellationToken)
         {
-            //return document;
             var statementInsideIf = (ReturnStatementSyntax)(ifStatement.Statement is BlockSyntax ? ((BlockSyntax)ifStatement.Statement).Statements.Single() : ifStatement.Statement);
             var elseStatement = ifStatement.Else;
             var statementInsideElse = (ReturnStatementSyntax)(elseStatement.Statement is BlockSyntax ? ((BlockSyntax)elseStatement.Statement).Statements.Single() : elseStatement.Statement);
