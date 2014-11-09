8a2916ae38055b2da7d0aada4842b4d6be4f0218
Vinicius Hana <viniciushana@outlook.com>
2014-11-09 20:42:03 -0200

    Adds analyzer for detecting unnecessary empty parenthesis

---
+++ src/CodeCracker/UnnecessaryParenthesisAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeCracker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class UnnecessaryParenthesisAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CodeCracker.UnnecessaryParenthesisAnalyzer";
+        internal const string Title = "Unnecessary Parenthesis";
+        internal const string MessageFormat = "{0}";
+        internal const string Category = "Syntax";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(Analyzer, SyntaxKind.ObjectCreationExpression);
+        }
+
+        private void Analyzer(SyntaxNodeAnalysisContext context)
+        {
+            var objectCreation = context.Node as ObjectCreationExpressionSyntax;
+
+            if (objectCreation.Initializer != null && !objectCreation.ArgumentList.Arguments.Any())
+            {
+                var diagnostic = Diagnostic.Create(Rule, objectCreation.ArgumentList.OpenParenToken.GetLocation(), "Remove unnecessary parenthesis.");
+                context.ReportDiagnostic(diagnostic);
+            }            
+        }
+    }
+}
---
+++ test/CodeCracker.Test/UnnecessaryParenthesisTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class UnnecessaryParenthesisTests : DiagnosticVerifier
+    {
+        [Fact]
+        public void ConstructorWithEmptyParenthesisWithInitializerTriggersFix()
+        {
+            const string source = @"var a = new B() { X = 1 };";
+            var expected = new DiagnosticResult
+            {
+                Id = UnnecessaryParenthesisAnalyzer.DiagnosticId,
+                Message = "Remove unnecessary parenthesis.",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 1, 14)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(source, expected);
+        }
+
+        [Fact]
+        public void ConstructorWithoutParenthesisWithInitializerIsIgnored()
+        {
+            const string source = @"new B { X = 1 };";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void ConstructorWithEmptyParenthesisWithoutInitializerIsIgnored()
+        {
+            const string source = @"new B();";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void ConstructorWithArgumentsWithInitializerIsIgnored()
+        {
+            const string source = @"new Sample(1) { A = 2 };";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void ConstructorWithArgumentsWithoutInitializerIsIgnored()
+        {
+            const string source = @"new Sample(1);";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void ConstructorWithoutArgumentsWithEmptyInitializerIsIgnored()
+        {
+            const string source = @"new Sample { };";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new UnnecessaryParenthesisAnalyzer();
+        }
+    }
+}
