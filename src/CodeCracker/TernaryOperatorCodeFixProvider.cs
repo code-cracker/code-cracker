12f314bbf7e7fb06463bcbbe2436f4770acad267
Giovanni Bassi <giggio@giggio.net>
2014-11-09 14:45:18 -0600

    support ternary operator in assignments, #7

--- src/CodeCracker/TernaryOperatorCodeFixProvider.cs
+++ src/CodeCracker/TernaryOperatorCodeFixProvider.cs
@@ -12,12 +12,12 @@ using System.Threading.Tasks;
 
 namespace CodeCracker
 {
-    [ExportCodeFixProvider("CodeCrackerTernaryOperatorCodeFixProvider", LanguageNames.CSharp), Shared]
-    public class TernaryOperatorCodeFixProvider : CodeFixProvider
+    [ExportCodeFixProvider("CodeCrackerTernaryOperatorWithReturnCodeFixProvider", LanguageNames.CSharp), Shared]
+    public class TernaryOperatorWithReturnCodeFixProvider : CodeFixProvider
     {
         public sealed override ImmutableArray<string> GetFixableDiagnosticIds()
         {
-            return ImmutableArray.Create(TernaryOperatorAnalyzer.DiagnosticId);
+            return ImmutableArray.Create(TernaryOperatorAnalyzer.DiagnosticIdForIfWithReturn);
         }
 
         public sealed override FixAllProvider GetFixAllProvider()
@@ -43,6 +43,50 @@ namespace CodeCracker
                 .WithLeadingTrivia(ifStatement.GetLeadingTrivia())
                 .WithTrailingTrivia(ifStatement.GetTrailingTrivia())
                 .WithAdditionalAnnotations(Formatter.Annotation);
+            var root = await document.GetSyntaxRootAsync();
+            var newRoot = root.ReplaceNode(ifStatement, ternary);
+            var newDocument = document.WithSyntaxRoot(newRoot);
+            return newDocument;
+        }
+    }
+
+    [ExportCodeFixProvider("CodeCrackerTernaryOperatorWithAssignmentCodeFixProvider", LanguageNames.CSharp), Shared]
+    public class TernaryOperatorWithAssignmentCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> GetFixableDiagnosticIds()
+        {
+            return ImmutableArray.Create(TernaryOperatorAnalyzer.DiagnosticIdForIfWithAssignment);
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task ComputeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<IfStatementSyntax>().First();
+            context.RegisterFix(CodeAction.Create("Change to ternary operator", c => MakeTernaryAsync(context.Document, declaration, c)), diagnostic);
+        }
+
+        private async Task<Document> MakeTernaryAsync(Document document, IfStatementSyntax ifStatement, CancellationToken cancellationToken)
+        {
+            var expressionInsideIf  = (ExpressionStatementSyntax)(ifStatement.Statement is BlockSyntax ? ((BlockSyntax)ifStatement.Statement).Statements.Single() : ifStatement.Statement);
+            var elseStatement = ifStatement.Else;
+            var expressionInsideElse  = (ExpressionStatementSyntax)(elseStatement.Statement is BlockSyntax ? ((BlockSyntax)elseStatement.Statement).Statements.Single() : elseStatement.Statement);
+
+            var assignmentExpressionInsideIf = (AssignmentExpressionSyntax)expressionInsideIf.Expression;
+            var assignmentExpressionInsideElse = (AssignmentExpressionSyntax)expressionInsideElse.Expression;
+            var variableIdentifierInsideIf = assignmentExpressionInsideIf.Left as IdentifierNameSyntax;
+
+            var ternary = SyntaxFactory.ParseStatement("\{variableIdentifierInsideIf.Identifier.Text} = \{ifStatement.Condition.ToString()} ? \{assignmentExpressionInsideIf.Right.ToString()} : \{assignmentExpressionInsideElse.Right.ToString()};")
+                .WithLeadingTrivia(ifStatement.GetLeadingTrivia())
+                .WithTrailingTrivia(ifStatement.GetTrailingTrivia())
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
             var root = await document.GetSyntaxRootAsync();
             var newRoot = root.ReplaceNode(ifStatement, ternary);
             var newDocument = document.WithSyntaxRoot(newRoot);
