237cf8fe2b0e79ab8aba046c710db7ea3bbeadc0
Vinicius Hana <viniciushana@outlook.com>
2014-11-09 22:23:10 -0200

    Adds verifier for empty object initializers.

---
+++ src/CodeCracker/EmptyObjectInitializerAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeCracker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class EmptyObjectInitializerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CodeCracker.EmptyObjectInitializerAnalyzer";
+        internal const string Title = "Empty Object Initializer";
+        internal const string MessageFormat = "{0}";
+        internal const string Category = "Syntax";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(Analyzer, SyntaxKind.ObjectCreationExpression);
+        }
+
+        private void Analyzer(SyntaxNodeAnalysisContext context)
+        {
+            var objectCreation = context.Node as ObjectCreationExpressionSyntax;
+
+            if (objectCreation.Initializer != null && !objectCreation.Initializer.Expressions.Any())
+            {
+                var diagnostic = Diagnostic.Create(Rule, objectCreation.Initializer.OpenBraceToken.GetLocation(), "Remove empty object initializer.");
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
---
+++ test/CodeCracker.Test/EmptyObjectInitializerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class EmptyObjectInitializerTests : CodeFixVerifier
+    {
+        [Fact]
+        public void EmptyObjectInitializerTriggersFix()
+        {
+            var code = @"var a = new A {};";
+            var expected = new DiagnosticResult
+            {
+                Id = EmptyObjectInitializerAnalyzer.DiagnosticId,
+                Message = "Remove empty object initializer.",
+                Severity = DiagnosticSeverity.Error,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 1, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(code, expected);
+        }
+
+        [Fact]
+        public void FilledObjectInitializerIsIgnored()
+        {
+            var code = @"var a = new A { X = 1 };";
+            VerifyCSharpHasNoDiagnostics(code);
+        }
+
+        [Fact]
+        public void AbsenceOfObjectInitializerIsIgnored()
+        {
+            var code = @"var a = new A();";
+            VerifyCSharpHasNoDiagnostics(code);
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new EmptyObjectInitializerAnalyzer();
+        }
+    }
+}
