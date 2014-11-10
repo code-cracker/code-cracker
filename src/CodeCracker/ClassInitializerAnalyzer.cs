6d0d21eb0a224d9a5dabefd3139a779c554037b6
Giovanni Bassi <giggio@giggio.net>
2014-11-10 10:47:49 -0200

    add class initializer for declaration with assignment, #10

---
+++ src/CodeCracker/ClassInitializerAnalyzer.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeCracker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ClassInitializerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticIdLocalDeclaration = "CodeCracker.ClassInitializerLocalDeclarationAnalyzer";
+        internal const string TitleLocalDeclaration = "Use class initializer";
+        internal const string MessageFormat = "{0}";
+        internal const string Category = "Syntax";
+        public const string DiagnosticIdAssignment = "CodeCracker.ClassInitializerAssignmentAnalyzer";
+        internal const string TitleAssignment = "Use class initializer";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticIdLocalDeclaration, TitleLocalDeclaration, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzerLocalDeclaration, SyntaxKind.LocalDeclarationStatement);
+            context.RegisterSyntaxNodeAction(AnalyzerAssignment, SyntaxKind.ExpressionStatement);
+        }
+
+        private void AnalyzerAssignment(SyntaxNodeAnalysisContext context)
+        {
+            var semanticModel = context.SemanticModel;
+            var expressionStatement = context.Node as ExpressionStatementSyntax;
+            if (!expressionStatement?.Expression?.IsKind(SyntaxKind.SimpleAssignmentExpression) ?? false) return;
+            var assignmentExpression = (AssignmentExpressionSyntax)expressionStatement.Expression;
+            var variableSymbol = semanticModel.GetSymbolInfo(assignmentExpression.Left).Symbol;
+
+            var blockParent = expressionStatement.FirstAncestorOrSelf<BlockSyntax>();
+            var isBefore = true;
+            var assignmentExpressions = new List<ExpressionStatementSyntax>();
+            foreach (var statement in blockParent.Statements)
+            {
+                if (isBefore)
+                {
+                    if (statement.Equals(expressionStatement)) isBefore = false;
+                }
+                else
+                {
+                    var theExpressionStatement = statement as ExpressionStatementSyntax;
+                    if (theExpressionStatement == null) break;
+                    var theAssignmentExpression = theExpressionStatement.Expression as AssignmentExpressionSyntax;
+                    if (theAssignmentExpression == null || !theAssignmentExpression.IsKind(SyntaxKind.SimpleAssignmentExpression)) break;
+                    var memberAccess = theAssignmentExpression.Left as MemberAccessExpressionSyntax;
+                    if (memberAccess == null || !memberAccess.IsKind(SyntaxKind.SimpleMemberAccessExpression)) break;
+                    var memberIdentifier = memberAccess.Expression as IdentifierNameSyntax;
+                    if (memberIdentifier == null) break;
+                    var propertyIdentifier = memberAccess.Name as IdentifierNameSyntax;
+                    if (propertyIdentifier == null) break;
+                    if (!semanticModel.GetSymbolInfo(memberIdentifier).Symbol.Equals(variableSymbol)) break;
+                    assignmentExpressions.Add(theExpressionStatement);
+                }
+            }
+            if (!assignmentExpressions.Any()) return;
+
+            var diagnostic = Diagnostic.Create(Rule, expressionStatement.GetLocation(), "You can use initializers in here.");
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        private void AnalyzerLocalDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var semanticModel = context.SemanticModel;
+            var localDeclarationStatement = context.Node as LocalDeclarationStatementSyntax;
+            if (localDeclarationStatement == null) return;
+            var objectCreationExpressions = localDeclarationStatement.DescendantNodes().OfType<ObjectCreationExpressionSyntax>().ToList();
+            if (objectCreationExpressions.Count != 1) return;
+            var objectCreationExpression = objectCreationExpressions.Single();
+            if (localDeclarationStatement.Declaration.Variables.Count > 1) return;
+            var variable = localDeclarationStatement.Declaration.Variables.Single();
+            var variableSymbol = semanticModel.GetDeclaredSymbol(variable);
+            var blockParent = localDeclarationStatement.FirstAncestorOrSelf<BlockSyntax>();
+            var isBefore = true;
+            var assignmentExpressions = new List<ExpressionStatementSyntax>();
+            foreach (var statement in blockParent.Statements)
+            {
+                if (isBefore)
+                {
+                    if (statement.Equals(localDeclarationStatement)) isBefore = false;
+                }
+                else
+                {
+                    var expressionStatement = statement as ExpressionStatementSyntax;
+                    if (expressionStatement == null) break;
+                    var assignmentExpression = expressionStatement.Expression as AssignmentExpressionSyntax;
+                    if (assignmentExpression == null || !assignmentExpression.IsKind(SyntaxKind.SimpleAssignmentExpression)) break;
+                    var memberAccess = assignmentExpression.Left as MemberAccessExpressionSyntax;
+                    if (memberAccess == null || !memberAccess.IsKind(SyntaxKind.SimpleMemberAccessExpression)) break;
+                    var memberIdentifier = memberAccess.Expression as IdentifierNameSyntax;
+                    if (memberIdentifier == null) break;
+                    var propertyIdentifier = memberAccess.Name as IdentifierNameSyntax;
+                    if (propertyIdentifier == null) break;
+                    if (!semanticModel.GetSymbolInfo(memberIdentifier).Symbol.Equals(variableSymbol)) break;
+                    assignmentExpressions.Add(expressionStatement);
+                }
+            }
+            if (!assignmentExpressions.Any()) return;
+
+            var diagnostic = Diagnostic.Create(Rule, localDeclarationStatement.GetLocation(), "You can use initializers in here.");
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
---
+++ test/CodeCracker.Test/ClassInitializerTests.cs
@@ -0,0 +1,281 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using TestHelper;
+using CodeCracker;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class ClassInitializerWithLocalDeclarationTests : CodeFixVerifier
+    {
+
+        [Fact]
+        public void WhenAssigningButNotCreatingAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                var p = 1;
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenDeclaringTwoVariablesAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                Person q = new Person(), r = new Person();
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenVariableIsDeclaredAndObjectIsCreatedButNoAssignmentsHappenLaterAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                var p = new Person();
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenVariableIsDeclaredAndObjectIsCreatedButOnlyUnrelatedDeclarationsHappenLaterAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                var p = new Person();
+                string a;
+                int i;
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenVariableIsDeclaredAndObjectIsCreatedButOnlyUnrelatedAssignmentsHappenLaterAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            private A a;
+            public int Foo()
+            {
+                var p = new Person();
+                a.Name = 1;
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenDeclaringAndCreatingObjectAndAssigningPropertiesThenAnalyzerCreatesDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                var p = new Person();
+                p.Name = ""Giovanni"";
+                p.Age = 25;
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = ClassInitializerAnalyzer.DiagnosticIdLocalDeclaration,
+                Message = "You can use initializers in here.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 8, 17) }
+            };
+            VerifyCSharpDiagnostic(source, expected);
+        }
+
+        [Fact]
+        public void WhenAssigningToPropertiesOfJustCreatedObjectChangeToClassInitializersFix()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                string a;
+                //some comment before
+                var p = new Person();
+                p.Name = ""Giovanni"";
+                p.Age = 25;
+                //some comment after
+                string b;
+            }
+        }
+    }";
+
+            var fixtest = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                string a;
+                //some comment before
+                var p = new Person()
+                {
+                    Name = ""Giovanni"",
+                    Age = 25
+                };
+                //some comment after
+                string b;
+            }
+        }
+    }";
+            VerifyCSharpFix(source, fixtest, 0);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new ClassInitializerCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ClassInitializerAnalyzer();
+        }
+    }
+
+    public class ClassInitializerWithAssingmentTests : CodeFixVerifier
+    {
+
+        [Fact]
+        public void WhenObjectIsCreatedButNoAssignmentsHappenLaterAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                Person p;
+                p = new Person();
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenCreatingObjectAndAssigningPropertiesThenAnalyzerCreatesDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                Person p;
+                p = new Person();
+                p.Name = ""Giovanni"";
+                p.Age = 25;
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = ClassInitializerAnalyzer.DiagnosticIdLocalDeclaration,
+                Message = "You can use initializers in here.",
+                Severity = DiagnosticSeverity.Error,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 9, 17) }
+            };
+            VerifyCSharpDiagnostic(source, expected);
+        }
+
+        [Fact]
+        public void WhenObjectIsCreatedButOnlyUnrelatedDeclarationsHappenLaterAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int Foo()
+            {
+                Person p;
+                p = new Person();
+                string a;
+                int i;
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        [Fact]
+        public void WhenObjectIsCreatedButOnlyUnrelatedAssignmentsHappenLaterAnalyzerDoesNotCreateDiagnostic()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            private A a;
+            public int Foo()
+            {
+                Person p;
+                p = new Person();
+                a.Name = 1;
+            }
+        }
+    }";
+            VerifyCSharpHasNoDiagnostics(source);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new ClassInitializerCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new ClassInitializerAnalyzer();
+        }
+    }
+}
