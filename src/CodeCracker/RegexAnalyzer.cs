7fe1b946ce7c66bd29fb3683137db00914d4901c
carloscds <{cds@cds-software.com.br}>
2014-11-17 10:56:56 -0200

    Regex

---
+++ src/CodeCracker/RegexAnalyzer.cs
@@ -0,0 +1,59 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+
+namespace CodeCracker
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class RegexAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CodeCracker.RegexAnalyzer";
+        internal const string Title = "Your Regex expression is wrong";
+        internal const string MessageFormat = "{0}";
+        internal const string Category = "Syntax";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(Analyzer, SyntaxKind.InvocationExpression);
+        }
+
+        private void Analyzer(SyntaxNodeAnalysisContext context)
+        {
+            var invocationExpression = (InvocationExpressionSyntax)context.Node;
+
+            var memberExpresion = invocationExpression.Expression as MemberAccessExpressionSyntax;
+            if (memberExpresion == null) return;
+            if (memberExpresion.Name.ToString() != "Match") return;
+
+            var memberSymbol = context.SemanticModel.GetSymbolInfo(memberExpresion).Symbol as IMethodSymbol;
+            if (!memberSymbol?.ToString().StartsWith("System.Text.RegularExpressions.Regex.Match") ?? true) return;
+
+            var argumentList = invocationExpression.ArgumentList as ArgumentListSyntax;
+            if ((argumentList?.Arguments.Count ?? 0) < 2) return;
+
+            var regexLiteral = argumentList.Arguments[1].Expression as LiteralExpressionSyntax;
+            if (regexLiteral == null) return;
+
+            var regexOpt = context.SemanticModel.GetConstantValue(regexLiteral);
+
+            var regex = regexOpt.Value as string;
+
+            try
+            {
+                System.Text.RegularExpressions.Regex.Match("", regex);
+            }
+            catch (ArgumentException e)
+            {
+                var diag = Diagnostic.Create(Rule, regexLiteral.GetLocation(), e.Message);
+                context.ReportDiagnostic(diag);
+            }
+        }
+    }
+}
