8f084a01c2fa594d343d6983677a43fabf12bd37
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2014-11-18 11:12:25 -0200

    AlwaysUseVar analyzer, codefix and tests

---
+++ test/CodeCracker.Test/AlwaysUseVarTests.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.CodeAnalysis;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class AlwaysUseVarTests
+         : CodeFixTest<AlwaysUseVarAnalyzer, AlwaysUseVarCodeFixProvider>
+    {
+        [Fact]
+        public void IgnoresConstantDeclarations()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                const int a = 10;
+            }
+        }
+    }";
+
+            VerifyCSharpHasNoDiagnostics(test);
+
+        }
+
+        [Fact]
+        public void IgnoresVarDeclarations()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                var a = 10;
+            }
+        }
+    }";
+
+            VerifyCSharpHasNoDiagnostics(test);
+
+        }
+
+        [Fact]
+        public void IgnoresDeclarationsWithNoInitializers()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                int a = 10, b;
+            }
+        }
+    }";
+
+            VerifyCSharpHasNoDiagnostics(test);
+
+        }
+
+        [Fact]
+        public void IgnoresDeclarationsWithNoIdentityConversions()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                IFee fee = new Fee();
+            }
+        }
+        interface IFee{}
+        class Fee: IFee {}
+    }";
+
+            VerifyCSharpHasNoDiagnostics(test);
+
+        }
+
+        [Fact]
+        public void CreateDiagnosticsWhenAssigningValueWithSameDeclaringType()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                int a = 10;
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = AlwaysUseVarAnalyzer.DiagnosticId,
+                Message = "Use 'var' instead specifying the type name.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 17) }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        [Fact]
+        public void FixReplacesDeclaringTypeWithVarIdentifier()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                int a = 10;
+            }
+        }
+    }";
+
+            const string expected = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                var a = 10;
+            }
+        }
+    }";
+            VerifyCSharpFix(test, expected);
+        }
+    }
+}
