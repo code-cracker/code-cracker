823b1a37c66c04a0c1ca5190c5e345abf75fc36e
Giovanni Bassi <giggio@giggio.net>
2014-11-20 23:34:35 -0200

    fix and rename nuspec file

--- buildNuget.cmd
+++ buildNuget.cmd
@@ -1,3 +1,5 @@
-pushd "%~dp0src\CodeCracker\bin\Debug"
-"%~dp0\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe" pack Diagnostic.nuspec -OutputDirectory "%~dp0\"
+pushd "%~dp0"
+set currDir=%~dp0
+set currDir=%currDir:~0,-1%
+"%currDir%\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe" pack "%currDir%\src\CodeCracker\CodeCracker.csproj" -Properties "Configuration=Debug;Platform=AnyCPU" -Symbols -OutputDirectory "%currDir%"
 popd
