1d27aeabb461bec39d748ce385528793d8a2cd6c
Giovanni Bassi <giggio@giggio.net>
2014-11-22 13:21:30 -0200

    dont nuget pack symbols, use nuspec not csproj
    Symbols are unnecessary as we are not providing a library
    If we pack the csproj it includes a lib folder as if we
    were packing a library and also incluides our package deps
    as deps in our package (but this could be disabled)

--- buildNuget.cmd
+++ buildNuget.cmd
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 set currDir=%~dp0
 set currDir=%currDir:~0,-1%
-"%currDir%\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe" pack "%currDir%\src\CodeCracker\CodeCracker.csproj" -Properties "Configuration=Debug;Platform=AnyCPU" -Symbols -OutputDirectory "%currDir%"
+"%currDir%\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe" pack "%currDir%\src\CodeCracker\CodeCracker.nuspec" -Properties "Configuration=Debug;Platform=AnyCPU" -OutputDirectory "%currDir%"
 popd
