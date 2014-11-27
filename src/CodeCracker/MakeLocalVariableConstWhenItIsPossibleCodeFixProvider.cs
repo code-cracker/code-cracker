3427a68e9bc18c42afc2384cd9dfb95927796924
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2014-11-27 16:55:32 -0200

    diagnostic, fixer and test for making variables constants whenever it's possible

---
+++ src/CodeCracker/MakeLocalVariableConstWhenItIsPossibleCodeFixProvider.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Simplification;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeCracker
+{
+    [ExportCodeFixProvider("CodeCrackerMakeLocalVariableConstWhenItIsPossibleCodeFixProvider", LanguageNames.CSharp), Shared]
+
+    public class MakeLocalVariableConstWhenItIsPossibleCodeFixProvider
+        : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> GetFixableDiagnosticIds()
+        {
+            return ImmutableArray.Create(MakeLocalVariableConstWhenItIsPossibleAnalyzer.DiagnosticId);
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task ComputeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+            var localDeclaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<LocalDeclarationStatementSyntax>().First();
+            var message = "Make constant";
+            context.RegisterFix(CodeAction.Create(message, c => MakeConstantAsync(context.Document, localDeclaration, c)), diagnostic);
+        }
+
+        private async Task<Document> MakeConstantAsync(Document document, LocalDeclarationStatementSyntax localDeclaration, CancellationToken cancellationToken)
+        {
+            var declaration = localDeclaration.Declaration;
+            var typeName = declaration.Type;
+
+            if (typeName.IsVar)
+            {
+                var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
+
+                // using var = ...
+                var aliasInfo = semanticModel.GetAliasInfo(typeName);
+                if (aliasInfo == null)
+                {
+                    var type = semanticModel.GetTypeInfo(typeName).ConvertedType;
+                    if (type.Name != "var")
+                    {
+                        var newtypeName = SyntaxFactory.ParseTypeName(type.ToDisplayString())
+                           .WithLeadingTrivia(typeName.GetLeadingTrivia())
+                           .WithTrailingTrivia(typeName.GetTrailingTrivia())
+                           .WithAdditionalAnnotations(Simplifier.Annotation);
+
+                        declaration = declaration.WithType(newtypeName);
+
+                    }
+                }
+            }
+
+            var @const = SyntaxFactory.Token(
+                localDeclaration.GetLeadingTrivia(),
+                SyntaxKind.ConstKeyword,
+                SyntaxFactory.TriviaList(SyntaxFactory.ElasticMarker)
+                );
+
+            var modifiers = localDeclaration.Modifiers.Insert(0, @const);
+
+            var newLocalDeclaration = localDeclaration
+                .WithLeadingTrivia(SyntaxTriviaList.Empty)
+                .WithModifiers(modifiers)
+                .WithDeclaration(declaration)
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
+
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(localDeclaration, newLocalDeclaration);
+
+            return document.WithSyntaxRoot(newRoot);
+        }
+    }
+}
---
+++ test/CodeCracker.Test/MakeLocalVariablesConstWhenItIsPossibleTests.cs
@@ -0,0 +1,379 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test
+{
+    public class MakeLocalVariablesConstWhenItIsPossibleTests : 
+        CodeFixTest<MakeLocalVariableConstWhenItIsPossibleAnalyzer, MakeLocalVariableConstWhenItIsPossibleCodeFixProvider>
+    {
+        [Fact]
+        public async Task IgnoresConstantDeclarations()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                const int a = 10;
+            }
+        }
+    }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+
+        }
+
+        [Fact]
+        public async Task IgnoresDeclarationsWithNoInitializers()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                int a;
+            }
+        }
+    }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresDeclarationsWithNonConstants()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                int a = GetValue();
+            }
+        }
+    }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresDeclarationsWithReferenceTypes()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                Foo a = new Foo();
+            }
+        }
+        class Foo {}
+    }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresVariablesThatChangesValueOutsideDeclaration()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                int a = 10;
+                a = 20;
+            }
+        }
+    }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        
+
+        [Fact]
+        public async Task CreateDiagnosticsWhenAssigningAPotentialConstant()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                int a = 10;
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = MakeLocalVariableConstWhenItIsPossibleAnalyzer.DiagnosticId,
+                Message = "This variables can be made const.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task CreateDiagnosticsWhenAssigningAPotentialConstantInAVarDeclaration()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                var a = 10;
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = MakeLocalVariableConstWhenItIsPossibleAnalyzer.DiagnosticId,
+                Message = "This variables can be made const.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task CreateDiagnosticsWhenAssigningNullToAReferenceType()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                Foo a = null;
+            }
+        }
+        class Foo {}
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = MakeLocalVariableConstWhenItIsPossibleAnalyzer.DiagnosticId,
+                Message = "This variables can be made const.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task FixMakesAVariableConstWhenDeclarationSpecifiesTypeName()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                int a = 10;
+            }
+        }
+    }";
+
+            const string expected = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                const int a = 10;
+            }
+        }
+    }";
+            await VerifyCSharpFixAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task FixMakesAVariableConstWhenDeclarationUsesVar()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                var a = 10;
+            }
+        }
+    }";
+
+            const string expected = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                const int a = 10;
+            }
+        }
+    }";
+            await VerifyCSharpFixAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task FixMakesAVariableConstWhenSettingNullToAReferenceType()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                Fee a = null;
+            }
+        }
+
+        class Fee {}
+    }";
+
+            const string expected = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                const Fee a = null;
+            }
+        }
+
+        class Fee {}
+    }";
+            await VerifyCSharpFixAsync(test, expected);
+        }
+
+
+        [Fact]
+        public async Task FixMakesAVariableConstWhenUsingVarAsAlias()
+        {
+            const string test = @"
+    using System;
+    using var = System.Int32;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                var a = 0;
+            }
+        }
+
+        class Fee {}
+    }";
+
+            const string expected = @"
+    using System;
+    using var = System.Int32;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                const var a = 0;
+            }
+        }
+
+        class Fee {}
+    }";
+            await VerifyCSharpFixAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task FixMakesAVariableConstWhenUsingVarAsClass()
+        {
+            const string test = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                var a = null;
+            }
+        }
+
+        class var {}
+    }";
+
+            const string expected = @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public async Task Foo()
+            {
+                const var a = null;
+            }
+        }
+
+        class var {}
+    }";
+            await VerifyCSharpFixAsync(test, expected);
+        }
+
+    }
+}
