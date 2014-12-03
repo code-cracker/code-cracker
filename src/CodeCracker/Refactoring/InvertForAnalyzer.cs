d6fc282d93d145495117e8830272ec5bc1cf263b
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2014-12-03 17:52:05 -0200

    analyzer, codefix and tests for 'Invert For' (closes #87)

---
+++ src/CodeCracker/Refactoring/InvertForAnalyzer.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace CodeCracker.Refactoring
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class InvertForAnalyzer
+        : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CC0042";
+        internal const string Title = "Invert the for loop counting.";
+        internal const string MessageFormat = "Make it a for loop that {0} the counter.";
+        internal const string Category = SupportedCategories.Refactoring;
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Hidden,
+            isEnabledByDefault: true,
+            helpLink: HelpLink.ForDiagnostic(DiagnosticId));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ForStatement);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var @for = (ForStatementSyntax)context.Node;
+
+            if (@for.Declaration == null && @for.Initializers.Count == 0)
+            {
+                return;
+            }
+
+            if (@for.Declaration != null && @for.Declaration.Variables.Count != 1)
+            {
+                return;
+            }
+
+            if (!HasAcceptableIncrementors(@for))
+            {
+                return;
+            }
+
+            if (!HasConditionCompatibleWithTheIncrementor(@for))
+            {
+                return;
+            }
+
+            if (!AreDeclarationConditionAndIncrementorUsingTheSameVariable(@for))
+            {
+                return;
+            }
+
+            var diagnostic = Diagnostic.Create(Rule, @for.GetLocation(),
+                IsPostIncrement(@for.Incrementors[0]) ? "decrement" : "increment"
+                );
+
+            context.ReportDiagnostic(diagnostic);
+            
+        }
+
+        internal static bool IsPostIncrement(ExpressionSyntax expression)
+        {
+            var unary = expression as PostfixUnaryExpressionSyntax;
+            if (unary == null) return false;
+
+            return unary.IsKind(SyntaxKind.PostIncrementExpression);
+        }
+
+        static bool HasAcceptableIncrementors(ForStatementSyntax @for)
+        {
+            if (@for.Incrementors.Count != 1)
+            {
+                return false;
+            }
+
+            var unary = @for.Incrementors[0] as PostfixUnaryExpressionSyntax;
+            if (unary == null)
+            {
+                return false;
+            }
+
+            return unary.IsKind(SyntaxKind.PostIncrementExpression) || unary.IsKind(SyntaxKind.PostDecrementExpression);
+        }
+
+        static bool HasConditionCompatibleWithTheIncrementor(ForStatementSyntax @for)
+        {
+            var condition = @for.Condition as BinaryExpressionSyntax;
+            var postIncrement = IsPostIncrement(@for.Incrementors[0]);
+
+            return
+                condition != null &&
+                (
+                    (postIncrement && condition.OperatorToken.IsKind(SyntaxKind.LessThanToken)) ||
+                    (!postIncrement && condition.OperatorToken.IsKind(SyntaxKind.GreaterThanEqualsToken))
+                );
+        }
+
+        static bool AreDeclarationConditionAndIncrementorUsingTheSameVariable(ForStatementSyntax @for)
+        {
+            SyntaxToken reference;
+
+            if (@for.Declaration != null)
+            {
+                reference = @for.Declaration.Variables[0].Identifier;
+            }
+            else
+            {
+                var initializer = @for.Initializers[0] as AssignmentExpressionSyntax;
+                if (initializer == null) return false;
+
+                var name = initializer.Left as IdentifierNameSyntax;
+                if (name == null) return false;
+
+                reference = name.Identifier;
+            }
+
+            var condition = @for.Condition as BinaryExpressionSyntax;
+            var incrementor = @for.Incrementors[0] as PostfixUnaryExpressionSyntax;
+
+            if (!(condition.Left is IdentifierNameSyntax))
+            {
+                return false;
+            }
+
+            if (!(incrementor.Operand is IdentifierNameSyntax))
+            {
+                return false;
+            }
+
+            var conditionVariableIdentifier = (condition.Left as IdentifierNameSyntax).Identifier;
+            var incrementorVariableIdentifier = (incrementor.Operand as IdentifierNameSyntax).Identifier;
+
+
+            return
+                reference.Text == conditionVariableIdentifier.Text &&
+                reference.Text == incrementorVariableIdentifier.Text;
+        }
+    }
+}
---
+++ test/CodeCracker.Test/Refactoring/InvertForTests.cs
@@ -0,0 +1,190 @@
+﻿using CodeCracker.Refactoring;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test.Refactoring
+{
+    public class InvertForTests :
+        CodeFixTest<InvertForAnalyzer, InvertForCodeFixProvider>
+    {
+        [Fact]
+        public async Task IgnoresWhenForUsesMoreThanOneIncrementor()
+        {
+            var test = _(@"for (var i = 0; i < n; i++, j++){}");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenForHasNoIncrementors()
+        {
+            var test = _(@"for (var i = 0; i < n; ){}");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenNotUsingPostfixIncrementOrDecrement()
+        {
+            var test = _(@"
+            for (var i = 0; i < n; i+=1){}
+            for (var i = 0; i < n; i-=1){}
+");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenUsingAnIncompatibleCondition()
+        {
+            var test = _(@"
+            for (var i = 0; true; i+=1){}
+            for (var i = 0; i >= n; i++){}
+            for (var i = 0; i < n; i--){}
+");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenUsingMoreThanOneDeclaration()
+        {
+            var test = _(@"
+            for (var i = 0, j = 2; i < n; i++){}
+");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenUsingNoDeclaration()
+        {
+            var test = _(@"
+            int i = 0;
+            for (; i < n; i++){}
+");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresIfDeclarationConditionOrIncrementorUsesADifferentVariable()
+        {
+            var test = _(@"
+            for (var i = 0; i < n; j++){}
+            for (var i = 0; j < n; i++){}
+            for (var j = 0; i < n; i++){}
+");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task FixReplacesFor0ToNWithNTo0()
+        {
+            var test = _(@"for (var i = 0; i < n; i++){}");
+            var fixtest = _(@"for (var i = n - 1; i >= 0; i--){}");
+            await VerifyCSharpFixAsync(test, fixtest, 0);
+        }
+
+        [Fact]
+        public async Task FixReplacesFor0ToNWithNTo0WithDeclaredVariables()
+        {
+            var test = _(@"int i; for (i = 0; i < n; i++){}");
+            var fixtest = _(@"int i; for (i = n - 1; i >= 0; i--){}");
+            await VerifyCSharpFixAsync(test, fixtest, 0);
+        }
+
+        [Fact]
+        public async Task FixReplacesForNTo0With0ToNWithDeclaredVariables()
+        {
+            var test = _(@"int i; for (i = n - 1; i >= 0; i--){}");
+            var fixtest = _(@"int i; for (i = 0; i < n; i++){}");
+            await VerifyCSharpFixAsync(test, fixtest, 0);
+        }
+
+        [Fact]
+        public async Task FixReplacesFor0ToNWithNTo0WithExplicitTyping()
+        {
+            var test = _(@"for (int i = 0; i < n; i++){}");
+            var fixtest = _(@"for (int i = n - 1; i >= 0; i--){}");
+            await VerifyCSharpFixAsync(test, fixtest, 0);
+        }
+
+        [Fact]
+        public async Task FixReplacesForAToBWithBToA()
+        {
+            var test = _(@"for (var i = a; i < b; i++){}");
+            var fixtest = _(@"for (var i = b - 1; i >= a; i--){}");
+            await VerifyCSharpFixAsync(test, fixtest, 0);
+        }
+
+        [Fact]
+        public async Task FixReplacesForNTo0With0ToN()
+        {
+            var test = _(@"for (var i = n - 1; i >= 0; i--){}");
+            var fixtest = _(@"for (var i = 0; i < n; i++){}"); 
+            await VerifyCSharpFixAsync(test, fixtest, 0);
+        }
+
+        [Fact]
+        public async Task CreateDiagnosticsWithForLoopsFrom0ToN()
+        {
+            var test = _(@"for (var i = 0; i < n; i++){}");
+
+            var expected = new DiagnosticResult
+            {
+                Id = InvertForAnalyzer.DiagnosticId,
+                Message = "Make it a for loop that decrement the counter.",
+                Severity = DiagnosticSeverity.Hidden,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task CreateDiagnosticsWithForLoopsTheUsesAnDeclaredVariableAsCounter()
+        {
+            var test = _(@"int i = 0; for (i = 0; i < n; i++){}");
+
+            var expected = new DiagnosticResult
+            {
+                Id = InvertForAnalyzer.DiagnosticId,
+                Message = "Make it a for loop that decrement the counter.",
+                Severity = DiagnosticSeverity.Hidden,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 28) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task CreateDiagnosticsWithForLoopsFromNTo0()
+        {
+            var test = _(@"for (var i = n - 1; i >= 0; i--){}");
+
+            var expected = new DiagnosticResult
+            {
+                Id = InvertForAnalyzer.DiagnosticId,
+                Message = "Make it a for loop that increment the counter.",
+                Severity = DiagnosticSeverity.Hidden,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 10, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        public string _(string code)
+        {
+            return @"
+    using System;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public void Foo()
+            {
+                " + code + @"
+            }
+        }
+    }";
+        }
+    }
+}
