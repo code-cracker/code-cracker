5df33f3a7a2420b1ade8b4a1fc1796fe74d3b954
Elemar Rodrigues Severo Junior <elemarjr@gmail.com>
2014-12-24 13:24:30 -0200

    Abstract Classes Should Not Have Public Constructors (closes #164)

---
+++ src/CSharp/CodeCracker/Usage/AbstractClassShouldNotHavePublicCtorsAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeCracker.Usage
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AbstractClassShouldNotHavePublicCtorsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CC0060";
+        internal const string Title = "Abastract class should not have public constructors.";
+        internal const string MessageFormat = "Constructor should not be public.";
+        internal const string Category = SupportedCategories.Usage;
+        
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            helpLink: HelpLink.ForDiagnostic(DiagnosticId));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ConstructorDeclaration);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var ctor = (ConstructorDeclarationSyntax)context.Node;
+            if (!ctor.Modifiers.Any(m => m.IsKind(SyntaxKind.PublicKeyword))) return;
+            
+            var @class = ctor.Ancestors().OfType<ClassDeclarationSyntax>().FirstOrDefault();
+            if (@class == null) return;
+            if (!@class.Modifiers.Any(m => m.IsKind(SyntaxKind.AbstractKeyword))) return;
+            
+            var diagnostic = Diagnostic.Create(Rule, ctor.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
---
+++ src/CSharp/CodeCracker/Usage/AbstractClassShouldNotHavePublicCtorsCodeFixProvider.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeCracker.Usage
+{
+    [ExportCodeFixProvider("CodeCrackerAbstractClassShouldNotHavePublicCtorsCodeFixProvider", LanguageNames.CSharp), Shared]
+
+    public class AbstractClassShouldNotHavePublicCtorsCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> GetFixableDiagnosticIds()
+        {
+            return ImmutableArray.Create(AbstractClassShouldNotHavePublicCtorsAnalyzer.DiagnosticId);
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task ComputeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            var ctor = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<ConstructorDeclarationSyntax>().First();
+
+            context.RegisterFix(CodeAction.Create("Use 'protected' instead of 'public'" , c => ReplacePublicWithProtectedAsync(context.Document, ctor, c)), diagnostic);
+            //foreach (var param in parameters)
+            //{
+            //    var message = "Use '" + param + "'";
+            //    context.RegisterFix(CodeAction.Create(message, c => FixParamAsync(context.Document, objectCreation, param, c)), diagnostic);
+            //}
+        }
+
+        private async Task<Document> ReplacePublicWithProtectedAsync(Document document, ConstructorDeclarationSyntax ctor, CancellationToken cancellationToken)
+        {
+            var @public = ctor.Modifiers.First(m => m.IsKind(SyntaxKind.PublicKeyword));
+            var @protected = SyntaxFactory.Token(@public.LeadingTrivia, SyntaxKind.ProtectedKeyword,
+                @public.TrailingTrivia);
+
+            var newModifiers = ctor.Modifiers.Replace(@public, @protected);
+            var newCtor = ctor.WithModifiers(newModifiers);
+            
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = root.ReplaceNode(ctor, newCtor);
+            var newDocument = document.WithSyntaxRoot(newRoot);
+            return newDocument;
+        }
+    }
+}
