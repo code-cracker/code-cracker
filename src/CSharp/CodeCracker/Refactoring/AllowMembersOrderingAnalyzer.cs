3bfea9ed3de17b081de637425e040309b765855d
dmgandini <dm.gandini@gmail.com>
2014-12-24 01:17:11 -0200

    Create AllowMembersOrdering Analyzer and its tests

---
+++ src/CSharp/CodeCracker/Refactoring/AllowMembersOrderingAnalyzer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+namespace CodeCracker.Refactoring
+{
+    public class AllowMembersOrderingAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CC0035";
+
+        internal const string Title = "Ordening member inside this type.";
+        internal const string MessageFormat = "Ordening member inside this type.";
+        internal const string Category = SupportedCategories.Style;
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Hidden,
+            isEnabledByDefault: true,
+            helpLink: HelpLink.ForDiagnostic(DiagnosticId));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get { return ImmutableArray.Create(Rule); }
+        }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.ClassDeclaration);
+            context.RegisterSyntaxNodeAction(Analyze, SyntaxKind.StructDeclaration);
+        }
+
+        private void Analyze(SyntaxNodeAnalysisContext context)
+        {
+            var typeDeclarationSyntax = context.Node as TypeDeclarationSyntax;
+
+            if (typeDeclarationSyntax == null) return;
+
+            var currentChildNodesOrder = typeDeclarationSyntax.ChildNodes();
+
+            if (currentChildNodesOrder.Count() > 1)
+                context.ReportDiagnostic(Diagnostic.Create(Rule, typeDeclarationSyntax.GetLocation()));
+        }
+    }
+}
---
+++ test/CSharp/CodeCracker.Test/Refactoring/AllowMembersOrderingAnalyzerTests.cs
@@ -0,0 +1,65 @@
+﻿using CodeCracker.Refactoring;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using TestHelper;
+using Xunit;
+namespace CodeCracker.Test.Refactoring
+{
+    public class AllowMembersOrderingAnalyzerTests : CodeFixVerifier
+    {
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new AllowMembersOrderingAnalyzer();
+        }
+
+        [Theory]
+        [InlineData("class")]
+        [InlineData("struct")]
+        public async void AllowMembersOrderingForEmptyTypeShouldNotTiggerDiagnostic(string typeDeclaration)
+        {
+            var test = @"
+            " + typeDeclaration + @" Foo
+            {
+            }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Theory]
+        [InlineData("class")]
+        [InlineData("struct")]
+        public async void AllowMembersOrderingForOneMemberShouldNotTiggerDiagnostic(string typeDeclaration)
+        {
+            var test = @"
+            " + typeDeclaration + @" Foo
+            {
+                int bar() { return 0; }
+            }";
+
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Theory]
+        [InlineData("class")]
+        [InlineData("struct")]
+        public async void AllowMembersOrderingForMoreThanOneMemberShouldTiggerDiagnostic(string typeDeclaration)
+        {
+            var test = @"
+            " + typeDeclaration + @" Foo
+            {
+                int bar() { return 0; }
+                void car() { }
+            }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = AllowMembersOrderingAnalyzer.DiagnosticId,
+                Message = AllowMembersOrderingAnalyzer.MessageFormat,
+                Severity = DiagnosticSeverity.Hidden,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 13) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+    }
+}
