aa57e8e82754bc3b97c144366c3adbf45745ee28
carloscds <cds@cds-software.com.br>
2015-01-04 20:40:23 -0200

    Add tests

---
+++ test/CSharp/CodeCracker.Test/Style/InterfaceNameTests.cs
@@ -0,0 +1,68 @@
+﻿using CodeCracker.Style;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test.Usage
+{
+    public class InterfaceNameTests : CodeFixTest<InterfaceNameAnalyzer, InterfaceNameCodeFixProvider>
+    {
+        [Fact]
+        public async Task InterfaceNameStartsWithLetterI()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public interface IFoo
+        {
+            public void Test();
+        }
+    }";
+            await VerifyCSharpHasNoDiagnosticsAsync(source);
+        }
+
+        [Fact]
+        public async Task InterfaceNameNotStartsWithLetterI()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public interface Foo
+        {
+            public void Test();
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = InterfaceNameAnalyzer.DiagnosticId,
+                Message = InterfaceNameAnalyzer.MessageFormat,
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 4, 9) }
+            };
+            await VerifyCSharpDiagnosticAsync(source, expected);
+        }
+
+        [Fact]
+        public async Task ChangeFieldAssignedOnDeclarationToReadonly()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public interface Foo
+        {
+            public void Test();
+        }
+    }";
+            const string fixtest = @"
+    namespace ConsoleApplication1
+    {
+        public interface IFoo
+        {
+            public void Test();
+        }
+    }";
+            await VerifyCSharpFixAsync(source, fixtest);
+        }
+    }
+}
