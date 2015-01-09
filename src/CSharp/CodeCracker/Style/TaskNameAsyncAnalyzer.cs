272f7b8640d7cd9a1a3d66259f3c8d69ed10c06e
Carlos dos Santos <{cds@cds-software.com.br}>
2015-01-09 15:51:04 -0200

    TaskNameAsync

---
+++ src/CSharp/CodeCracker/Style/TaskNameAsyncAnalyzer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace CodeCracker.Style
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class TaskNameAsyncAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "CC0061";
+        internal const string Title = "Async method can be terminating with 'Async' name.";
+        internal const string MessageFormat = "Change method name including 'Async'.";
+        internal const string Category = SupportedCategories.Style;
+        const string Description = "Async method can be terminating with 'Async' name.";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId,
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true,
+            description: Description,
+            helpLink: HelpLink.ForDiagnostic(DiagnosticId));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeClass, SyntaxKind.MethodDeclaration);
+        }
+
+        public Task<bool> xpto()
+        {
+            return null;
+        }
+
+        private static void AnalyzeClass(SyntaxNodeAnalysisContext context)
+        {
+            var invocationExpression = (MethodDeclarationSyntax)context.Node;
+
+            var nameSyntax = invocationExpression.ReturnType?.ToString();
+            if (nameSyntax == null || !nameSyntax.Contains("Task")) return;
+            if(invocationExpression.Identifier.ToString().ToUpper().EndsWith("ASYNC")) return;
+
+            var error = string.Format(MessageFormat, MessageFormat);
+            var diag = Diagnostic.Create(Rule, invocationExpression.GetLocation(), error);
+            context.ReportDiagnostic(diag);
+        }
+    }
+}
---
+++ test/CSharp/CodeCracker.Test/Style/TaskNameASyncTests.cs
@@ -0,0 +1,109 @@
+﻿using CodeCracker.Style;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using TestHelper;
+using Xunit;
+
+namespace CodeCracker.Test.Style
+{
+    public class TaskNameAsyncTests : CodeFixTest<TaskNameAsyncAnalyzer, TaskNameAsyncCodeFixProvider>
+    {
+        [Fact]
+        public async Task TaskNameAsyncMethodCorrect()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {
+            Task TestASync() {};
+        }
+    }";
+            await VerifyCSharpHasNoDiagnosticsAsync(source);
+        }
+
+        [Fact]
+        public async Task TaskNameAsyncMethodWhithoutAsyncName()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {
+            Task Test() {};
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = TaskNameAsyncAnalyzer.DiagnosticId,
+                Message = TaskNameAsyncAnalyzer.MessageFormat,
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 6, 13) }
+            };
+            await VerifyCSharpDiagnosticAsync(source, expected);
+        }
+
+        [Fact]
+        public async Task ChangeTaskNameWhithoutAsync()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {
+            Task Test() {};
+        }
+    }";
+            const string fixtest = @"
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {
+            Task TestAsync() {};
+        }
+    }";
+            await VerifyCSharpFixAsync(source, fixtest);
+        }
+
+
+        [Fact]
+        public async Task ChangeTaskNameWhithoutAsyncAndClassImplementation()
+        {
+            const string source = @"
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {
+            public methodTest()
+            {
+                await Test();
+            }
+
+            public Task<bool> Test()
+            {
+                return true;
+            }
+        }
+
+    }";
+            const string fixtest = @"
+    namespace ConsoleApplication1
+    {
+        public class Foo
+        {
+            public methodTest()
+            {
+                await TestAsync();
+            }
+
+            public Task<bool> TestAsync()
+            {
+                return true;
+            }
+        }
+
+    }";
+            await VerifyCSharpFixAsync(source, fixtest);
+        }
+    }
+}
