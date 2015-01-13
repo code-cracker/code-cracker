e6d4e897a7073d28551c1f275fcd72ebffd80f3d
Giovanni Bassi <giggio@giggio.net>
2015-01-12 23:06:18 -0200

    build with psake, closes #215

---
+++ build.ps1
@@ -0,0 +1,11 @@
+if ((Test-Path $PSScriptRoot\packages\psake.4.4.1\tools\psake.psm1) -ne $true) {
+    nuget restore $PSScriptRoot\.nuget\packages.config -SolutionDirectory $PSScriptRoot
+}
+Import-Module $PSScriptRoot\packages\psake.4.4.1\tools\psake.psm1 -force
+if ($MyInvocation.UnboundArguments.Count -ne 0) {
+    . $PSScriptRoot\psake.ps1 -taskList ($MyInvocation.UnboundArguments -join " ")
+}
+else {
+    . $PSScriptRoot\build.ps1 Build
+}
+exit !($psake.build_success)
