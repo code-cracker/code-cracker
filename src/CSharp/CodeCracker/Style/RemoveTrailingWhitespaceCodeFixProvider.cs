f7b80cb0b2b8f803f1a3a829ecc32db364afae79
Giovanni Bassi <giggio@giggio.net>
2015-01-20 11:35:09 -0200

    remove trailing whitespace analyzer and code fix
    I used a syntax tree analyzer to be able to be performant
    Closes #201

---
+++ src/CSharp/CodeCracker/Style/RemoveTrailingWhitespaceCodeFixProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CodeCracker.Style
+{
+    [ExportCodeFixProvider("CodeCrackerRemoveTrailingWhitespaceCodeFixProvider", LanguageNames.CSharp), Shared]
+    public class RemoveTrailingWhitespaceCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> GetFixableDiagnosticIds() => ImmutableArray.Create(RemoveTrailingWhitespaceAnalyzer.DiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public sealed override async Task ComputeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var diagnostic = context.Diagnostics.First();
+            var trivia = root.FindTrivia(diagnostic.Location.SourceSpan.End - 1);
+            var newRoot = RemoveTrailingWhiteSpace(root, trivia);
+            context.RegisterFix(CodeAction.Create("Remove trailing whitespace", context.Document.WithSyntaxRoot(newRoot)), diagnostic);
+        }
+
+        private static SyntaxNode RemoveTrailingWhiteSpace(SyntaxNode root, SyntaxTrivia trivia)
+        {
+            SyntaxNode newRoot;
+            if (trivia.IsKind(SyntaxKind.WhitespaceTrivia))
+            {
+                newRoot = root.ReplaceTrivia(trivia, new SyntaxTrivia[] { });
+            }
+            else
+            {
+                var triviaNoTrailingWhiteSpace = Regex.Replace(trivia.ToFullString(), @"\s+$", "");
+                newRoot = root.ReplaceTrivia(trivia, SyntaxFactory.ParseTrailingTrivia(triviaNoTrailingWhiteSpace));
+            }
+            return newRoot;
+        }
+    }
+}
