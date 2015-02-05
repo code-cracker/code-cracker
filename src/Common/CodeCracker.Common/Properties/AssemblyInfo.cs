2a651560f59993e705b38441405f266176da86b2
Giovanni Bassi <giggio@giggio.net>
2015-02-05 09:42:07 -0200

    create common project and add diagnosticid to it
    Closes #248

---
+++ src/Common/CodeCracker.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CodeCracker.Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CodeCracker")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: InternalsVisibleTo("CodeCracker.Test.CSharp")]
+[assembly: InternalsVisibleTo("CodeCracker.Test.VisualBasic")]
