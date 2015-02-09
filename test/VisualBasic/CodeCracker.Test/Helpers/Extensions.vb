5f6c2a2dc362e2cd8458074135c7826fffed4311
Jim Wooley <JimWooley@hotmail.com>
2015-02-08 22:23:56 -0500

    Translate ConfigureAwait fix for VB

--- test/VisualBasic/CodeCracker.Test/Helpers/Extensions.vb
+++ test/VisualBasic/CodeCracker.Test/Helpers/Extensions.vb
@@ -2,17 +2,21 @@
 
 Module CodeFixTestExtensions
     <Extension>
-    Public Function WrapInMethod(code As String) As String
-        Return "
+    Public Function WrapInMethod(code As String, Optional isAsync As Boolean = False) As String
+        Return String.Format("
 Imports System
 Namespace ConsoleApplication1
     Class TypeName
-        Public Sub Foo()
-            the_code
+        Public {1}{2} Foo(){3}
+            {0}
             ' VB Requires value to be used or another analyzer is added which breaks the tests
             Console.WriteLine(a)
-        End Sub
+        End {2}
     End Class
-End Namespace".Replace("the_code", code)
+End Namespace",
+                             code,
+                             If(isAsync, "Async ", ""),
+                             If(isAsync, "Function", "Sub"),
+                             If(isAsync, " As Task", ""))
     End Function
 End Module
