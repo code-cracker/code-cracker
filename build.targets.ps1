e554cb8fbfd0c512f9e5341d5a290f1114c2f428
Giovanni Bassi <giggio@giggio.net>
2015-02-20 23:06:49 -0200

    remove build reporting, use appveyor's

--- build.targets.ps1
+++ build.targets.ps1
@@ -121,18 +121,6 @@ Task Pack-Nuget-VB -precondition { return $isAppVeyor } {
     PackNuget "VB" "$rootDir\src\VisualBasic" $nuspecPathVB $nupkgPathVB
 }
 
-Task Report-PR -precondition { return $isPullRequest } {
-    $body = ConvertTo-Json @{issueNumber=$env:APPVEYOR_PULL_REQUEST_NUMBER; body="Build $env:buildResult! Details: https://ci.appveyor.com/project/code-cracker/code-cracker/build/1.0.0.$env:APPVEYOR_BUILD_NUMBER"}
-    $uri = "https://codecrackerbot.herokuapp.com/message"
-    Invoke-RestMethod -Uri $uri -Method POST -Body $body -ContentType "application/json"
-}
-
-Task Report-Build-Failure -precondition { return $isPullRequest -ne $true } {
-    $body = ConvertTo-Json @{sha=$env:APPVEYOR_REPO_COMMIT; body="Build failed! Details: https://ci.appveyor.com/project/code-cracker/code-cracker/build/1.0.0.$env:APPVEYOR_BUILD_NUMBER"}
-    $uri = "https://codecrackerbot.herokuapp.com/message"
-    Invoke-RestMethod -Uri $uri -Method POST -Body $body -ContentType "application/json"
-}
-
 function PackNuget($language, $dir, $nuspecFile, $nupkgFile) {
     Write-Host "Packing nuget for $language..."
     [xml]$xml = cat $nuspecFile
