16375b2fb91639800fb406edb9c65274960eee6b
Giovanni Bassi <giggio@giggio.net>
2015-02-22 00:34:29 -0300

    test fix all for project on removeunreacheablecode

--- test/CSharp/CodeCracker.Test/Usage/RemoveUnreachableCodeTest.cs
+++ test/CSharp/CodeCracker.Test/Usage/RemoveUnreachableCodeTest.cs
@@ -308,5 +308,47 @@ class Foo
 }";
             await VerifyFixAllAsync(source, fixtest);
         }
+
+        [Fact]
+        public async void FixAllProjectUnreacheableCode()
+        {
+            const string source1 = @"
+class Foo
+{
+    void Method()
+    {
+        return;
+        var a = 1;
+        var b = 1;
+    }
+}";
+            const string source2 = @"
+class Foo2
+{
+    void Method()
+    {
+        return;
+        var a = 1;
+        var b = 1;
+    }
+}";
+            const string fixtest1 = @"
+class Foo
+{
+    void Method()
+    {
+        return;
+    }
+}";
+            const string fixtest2 = @"
+class Foo2
+{
+    void Method()
+    {
+        return;
+    }
+}";
+            await VerifyFixAllAsync(new[] { source1, source2 }, new[] { fixtest1, fixtest2 });
+        }
     }
 }
