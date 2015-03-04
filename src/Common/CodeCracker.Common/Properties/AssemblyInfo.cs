6d6b595b37e0366f3d5b156cc8d3f9e8b001f56b
Giovanni Bassi <giggio@giggio.net>
2015-03-03 23:52:49 -0300

    update versions to alpha4

--- src/Common/CodeCracker.Common/Properties/AssemblyInfo.cs
+++ src/Common/CodeCracker.Common/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.CompilerServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
 [assembly: InternalsVisibleTo("CodeCracker.Test.CSharp")]
 [assembly: InternalsVisibleTo("CodeCracker.Test.VisualBasic")]
--- test/CSharp/CodeCracker.Test/Properties/AssemblyInfo.cs
+++ test/CSharp/CodeCracker.Test/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Resources;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyTitle("CodeCracker.Test")]
+[assembly: AssemblyTitle("CodeCracker.Test.CSharp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
