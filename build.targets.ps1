a85d679cee16bd98a25ecf8154930b3cfcb35d9b
Giovanni Bassi <giggio@giggio.net>
2015-03-22 23:15:35 -0300

    update coveralls.io

--- build.targets.ps1
+++ build.targets.ps1
@@ -25,7 +25,7 @@ Properties {
     $outputXml = "$logDir\CodeCoverageResults.xml"
     $reportGeneratorExe = "$packagesDir\ReportGenerator.2.1.1.0\ReportGenerator.exe"
     $coverageReportDir = "$logDir\codecoverage\"
-    $converallsNetExe = "$packagesDir\coveralls.io.1.1.91\tools\coveralls.net.exe"
+    $converallsNetExe = "$packagesDir\coveralls.io.1.3.2\tools\coveralls.net.exe"
     $isRelease = $isAppVeyor -and ($env:APPVEYOR_REPO_BRANCH -eq "release")
     $isPullRequest = $env:APPVEYOR_PULL_REQUEST_NUMBER -ne $null
 }
