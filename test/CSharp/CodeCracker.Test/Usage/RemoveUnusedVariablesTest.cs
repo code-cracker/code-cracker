dd794a460032f37a0dce720522babfb0cc63bb2c
Juliano Sales <juliano-sales@hotmail.com>
2015-03-26 02:15:24 -0300

    Remove the Analyzer Unused Variables and updated fix

--- test/CSharp/CodeCracker.Test/Usage/RemoveUnusedVariablesTest.cs
+++ test/CSharp/CodeCracker.Test/Usage/RemoveUnusedVariablesTest.cs
@@ -5,46 +5,77 @@ using Xunit;
 
 namespace CodeCracker.Test.CSharp.Usage
 {
-    public class RemoveUnusedVariablesTest : CodeFixVerifier<RemoveUnusedVariablesAnalyzer, RemoveUnusedVariablesCodeFixProvider>
+    public class RemoveUnusedVariablesTest : CodeFixVerifier
     {
+        protected override CodeFixProvider GetCodeFixProvider() => new RemoveUnusedVariablesCodeFixProvider();
+
         [Fact]
-        public async Task WhenVariableUsedWithinOfMethodDoesNotShouldCreateDiagnostic()
+        public async Task WhenVariableIsAssignedButItsValueIsNeverUsedShouldCreateDiagnostics()
         {
             const string source = @"
-    class TypeName
-    {
-        public int  Foo()
+        class TypeName
         {
-            int a = 10;
-            return a;
-        }
-    }";
-            await VerifyCSharpHasNoDiagnosticsAsync(source);
+            public void Foo()
+            {
+                int a = 10;
+                return;
+            }
+        }";
+            const string fixtest = @"
+        class TypeName
+        {
+            public void Foo()
+            {
+                return;
+            }
+        }";
+
+            await VerifyCSharpFixAsync(source, fixtest);
         }
 
         [Fact]
-        public async Task WhenVariableUsedWithinOfConstructorDoesNotShouldCreateDiagnostic()
+        public async Task WhenVariableIsCreatedButNeverUsedShouldCreateDiagnostics()
         {
-            const string source = @"
-class Name
-{
-	public Name()
-	{
-		var number = 2;
 
-		var result = number * 3;
-        
-        return result;
-	}
-}";
-            await VerifyCSharpHasNoDiagnosticsAsync(source);
+            const string source = @"
+        class TypeName
+        {
+            public void Foo()
+            {
+                int a;
+                return;
+            }
+        }";
+            const string fixtest = @"
+        class TypeName
+        {
+            public void Foo()
+            {
+                return;
+            }
+        }";
+
+            await VerifyCSharpFixAsync(source, fixtest);
         }
 
         [Fact]
         public async Task WhenVariableWithinOfMethodDoesNotUsedShouldCreateDiagnostics()
         {
-            const string source = @"class Name{public void NewFoo(){int number = 2;}}";
-            const string fixtest = @"class Name{public void NewFoo(){}}";
+            const string source = @"
+        class Name
+        {
+            public void NewFoo()
+            {
+                int number = 2;
+            }
+        }";
+            const string fixtest = @"
+        class Name
+        {
+            public void NewFoo()
+            {
+            }
+        }";
 
             await VerifyCSharpFixAsync(source, fixtest);
         }
@@ -52,8 +83,22 @@ class Name
         [Fact]
         public async Task WhenVariableWithinOfConstructorDoesNotUsedShouldCreateDiagnostics()
         {
-            const string source = @"class Name{public Name(){var foo = 2;}}";
-            const string fixtest = @"class Name{public Name(){}}";
+            const string source = @"
+        class Name
+        {
+            public Name()
+            {
+                var foo = 2;
+            }
+        }";
+            const string fixtest = @"
+        class Name
+        {
+            public Name()
+            {
+            }
+        }";
+
 
             await VerifyCSharpFixAsync(source, fixtest);
         }
