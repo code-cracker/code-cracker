0a99ed8f0547c588cbee9594a2a377a3e2a375ee
Andre Carlucci <andrecarlucci@gmail.com>
2015-03-26 22:28:30 -0300

    VirtualMethodOnConstructorAnalyzer class added refs  #203

---
+++ src/CSharp/CodeCracker/Usage/VirtualMethodOnConstructorAnalyzer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeCracker.CSharp.Usage
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class VirtualMethodOnConstructorAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly LocalizableString Title = "Virtual Method Called On Constructor";
+        internal const string Message = "Do not call overridable methods in constructors";
+        internal const string Category = SupportedCategories.Usage;
+        const string Description = "When a virtual method is called, the actual type that executes the method "+
+                                   "is not selected until run time. When a constructor calls a virtual method, "+
+                                   "it is possible that the constructor for the instance that invokes the method "+
+                                   "has not executed.";
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId.VirtualMethodOnConstructor.ToDiagnosticId(),
+            Title, 
+            Message, 
+            Category, 
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true,
+            description: Description,
+            helpLinkUri: HelpLink.ForDiagnostic(DiagnosticId.VirtualMethodOnConstructor));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.ConstructorDeclaration);
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context) {
+
+        }
+    }
+}
