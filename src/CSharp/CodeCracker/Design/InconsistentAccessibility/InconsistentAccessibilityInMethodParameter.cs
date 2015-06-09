5ac12d6580a11ab14bbae3854c67908bb6cac383
baks <arkadiusz.komisarczuk@gmail.com>
2015-06-09 08:33:32 +0200

    InconsistentAccessibility CS0051 code fix
    Initial structure for other inconsistent accessibility related errors.
    
    CodeFix should return proper InconsistentAccessibilityInfoProvider
    depeneding on compiler error number. Provider should take care of
    finding information about type, new accessibility modifiers and code
    action message and return this information in
    InconsistentAccessibilityInfo object.
    
    Also added extension methods for MemberDeclarationSyntax

---
+++ src/CSharp/CodeCracker/Design/InconsistentAccessibility/InconsistentAccessibilityInMethodParameter.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodeCracker.CSharp.Design.InconsistentAccessibility
+{
+    public sealed class InconsistentAccessibilityInMethodParameter : InconsistentAccessibilityInfoProvider
+    {
+        public InconsistentAccessibilityInfo GetInconsistentAccessibilityInfo(SyntaxNode syntaxRoot, Diagnostic diagnostic)
+        {
+            var result = new InconsistentAccessibilityInfo();
+
+            var methodThatRaisedError = syntaxRoot.FindNode(diagnostic.Location.SourceSpan).DescendantNodesAndSelf().OfType<BaseMethodDeclarationSyntax>().FirstOrDefault();
+            if (methodThatRaisedError != null)
+            {
+                var parameterTypeFromMessage = ExtractParameterTypeFromErrorMessage(diagnostic.ToString());
+
+                var parameterTypeName = parameterTypeFromMessage;
+                var parameterTypeDotIndex = parameterTypeName.LastIndexOf('.');
+                if (parameterTypeDotIndex > 0)
+                {
+                    parameterTypeName = parameterTypeName.Substring(parameterTypeDotIndex + 1);
+                }
+
+                result.CodeActionMessage = $"Change parameter type '{parameterTypeFromMessage}' accessibility to be as accessible as method '{GetMethodIdentifierValueText(methodThatRaisedError)}'";
+                result.NewAccessibilityModifiers = CreateAccessibilityModifiersFromMethod(methodThatRaisedError);
+                result.InconsistentAccessibilityTypeName = parameterTypeName;
+            }
+
+            return result;
+        }
+
+        private static string ExtractParameterTypeFromErrorMessage(string compilerErrorMessage)
+        {
+            const int parameterTypeStartShift = 52;
+
+            var parameterTypeNameStart = compilerErrorMessage.IndexOf(InconsistentAccessibilityCodeFixProvider.InconsistentAccessibilityInMethodParameterCompilerErrorNumber, StringComparison.Ordinal) + parameterTypeStartShift;
+            var parameterTypeNameLength = compilerErrorMessage.IndexOf('\'', parameterTypeNameStart) - parameterTypeNameStart;
+            return compilerErrorMessage.Substring(parameterTypeNameStart, parameterTypeNameLength);
+        }
+
+        private static string GetMethodIdentifierValueText(BaseMethodDeclarationSyntax method) => method.IsKind(SyntaxKind.MethodDeclaration) ? ((MethodDeclarationSyntax)method).Identifier.ValueText : ((ConstructorDeclarationSyntax)method).Identifier.ValueText;
+
+        private static SyntaxTokenList CreateAccessibilityModifiersFromMethod(BaseMethodDeclarationSyntax method)
+        {
+            var modifiers = method.Modifiers;
+            if (method.Parent.IsKind(SyntaxKind.InterfaceDeclaration))
+            {
+                modifiers = ((InterfaceDeclarationSyntax)method.Parent).Modifiers;
+            }
+
+            var accessibilityModifiers = modifiers.Where(token => token.IsKind(SyntaxKind.PublicKeyword, SyntaxKind.ProtectedKeyword, SyntaxKind.InternalKeyword)).Select(token => SyntaxFactory.Token(token.Kind()));
+
+            return SyntaxFactory.TokenList(EnsureProtectedBeforeInternal(accessibilityModifiers));
+        }
+
+        private static IEnumerable<SyntaxToken> EnsureProtectedBeforeInternal(IEnumerable<SyntaxToken> modifiers) => modifiers.OrderByDescending(token => token.RawKind);
+    }
+}
---
+++ src/CSharp/CodeCracker/Design/InconsistentAccessibility/InconsistentAccessibilityInfo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CodeCracker.CSharp.Design.InconsistentAccessibility
+{
+    public class InconsistentAccessibilityInfo
+    {
+        public string CodeActionMessage { get; set; }
+        public string InconsistentAccessibilityTypeName { get; set; }
+        public SyntaxTokenList NewAccessibilityModifiers { get; set; }
+    }
+}
