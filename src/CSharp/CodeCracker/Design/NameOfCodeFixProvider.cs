b84d1a58143b7f75fb60b907302af3553c63476d
Giovanni Bassi <giggio@giggio.net>
2015-06-15 09:06:36 -0300

    add localization to NameOf analyzer and fix
    Related to #360

--- src/CSharp/CodeCracker/Design/NameOfCodeFixProvider.cs
+++ src/CSharp/CodeCracker/Design/NameOfCodeFixProvider.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using CodeCracker.Properties;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
@@ -18,11 +19,12 @@ namespace CodeCracker.CSharp.Design
         public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DiagnosticId.NameOf.ToDiagnosticId());
 
         public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+        internal static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.NameOfCodeFixProvider_Title), Resources.ResourceManager, typeof(Resources));
 
         public sealed override Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var diagnostic = context.Diagnostics.First();
-            context.RegisterCodeFix(CodeAction.Create("Use nameof()", c => MakeNameOfAsync(context.Document, diagnostic, c)), diagnostic);
+            context.RegisterCodeFix(CodeAction.Create(Title.ToString(), c => MakeNameOfAsync(context.Document, diagnostic, c)), diagnostic);
             return Task.FromResult(0);
         }
 
