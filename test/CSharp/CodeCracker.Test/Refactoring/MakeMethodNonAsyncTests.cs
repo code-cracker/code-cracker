f814d73f7e8db72ecb5cedbada5179f38f9da90b
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2015-06-25 16:43:58 +0200

    Fix #393: make async method non async
    if it lacks the await keyword.
    
    There is only a codefix, no analyzer, since it's based on the compiler's
    CS1998 warning. The codefix just removes the async keyword and replaces
    all occurrences of `return x` with return `Task.FromResult(x)`.

---
+++ test/CSharp/CodeCracker.Test/Refactoring/MakeMethodNonAsyncTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using CodeCracker.CSharp.Refactoring;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Xunit;
+
+namespace CodeCracker.Test.CSharp.Refactoring
+{
+    public class MakeMethodNonAsyncTests : CodeFixVerifier
+    {
+        [Fact]
+        public async Task ShouldRemoveAsyncKeywordAndReplaceReturnedValuesWithTaskFromResultAsync()
+        {
+            var codeFileTemplate = @"using System.Threading.Tasks;
+class Test
+{{
+{0}
+}}";
+
+            var testMethod = @"
+public static async Task<int> FooAsync()
+{
+    return 42;
+}";
+            var testCode = string.Format(codeFileTemplate, testMethod);
+            var fixedMethod = @"
+public static Task<int> FooAsync()
+{
+    return Task.FromResult(42);
+}";
+            var fixedCode = string.Format(codeFileTemplate, fixedMethod);
+
+            await this.VerifyCSharpFixAsync(testCode, fixedCode).ConfigureAwait(false);
+        }
+
+        protected override CodeFixProvider GetCodeFixProvider() => new MakeMethodNonAsyncCodeFixProvider();
+    }
+}
