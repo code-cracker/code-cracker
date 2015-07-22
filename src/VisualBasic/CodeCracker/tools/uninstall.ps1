b047437c0d980219932c3a61f29f99e804c1e4d1
Giovanni Bassi <giggio@giggio.net>
2015-07-21 22:42:35 -0300

    Update dependencies
    Change dependencies in main projects
    to "Copy Local=False"
    Test projects now target .NET 4.5.2
    Removed psake pkg reference from main projects
    Downgrade Microsoft.Composition to 1.0.27, and
    removed app.config files

--- src/VisualBasic/CodeCracker/tools/uninstall.ps1
+++ src/VisualBasic/CodeCracker/tools/uninstall.ps1
@@ -1,6 +1,56 @@
 ﻿param($installPath, $toolsPath, $package, $project)
 
-$analyzerPath = join-path $toolsPath "analyzers"
-$analyzerFilePath = join-path $analyzerPath "CodeCracker.VisualBasic.dll"
+$analyzersPaths = Join-Path (Join-Path (Split-Path -Path $toolsPath -Parent) "analyzers" ) * -Resolve
 
-$project.Object.AnalyzerReferences.Remove("$analyzerFilePath")
+foreach($analyzersPath in $analyzersPaths)
+{
+    # Uninstall the language agnostic analyzers.
+    if (Test-Path $analyzersPath)
+    {
+        foreach ($analyzerFilePath in Get-ChildItem $analyzersPath -Filter *.dll)
+        {
+            if($project.Object.AnalyzerReferences)
+            {
+                $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+            }
+        }
+    }
+}
+
+# $project.Type gives the language name like (C# or VB.NET)
+$languageFolder = ""
+if($project.Type -eq "C#")
+{
+    $languageFolder = "cs"
+}
+if($project.Type -eq "VB.NET")
+{
+    $languageFolder = "vb"
+}
+if($languageFolder -eq "")
+{
+    return
+}
+
+foreach($analyzersPath in $analyzersPaths)
+{
+    # Uninstall language specific analyzers.
+    $languageAnalyzersPath = join-path $analyzersPath $languageFolder
+    if (Test-Path $languageAnalyzersPath)
+    {
+        foreach ($analyzerFilePath in Get-ChildItem $languageAnalyzersPath -Filter *.dll)
+        {
+            if($project.Object.AnalyzerReferences)
+            {
+                try
+                {
+                    $project.Object.AnalyzerReferences.Remove($analyzerFilePath.FullName)
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
