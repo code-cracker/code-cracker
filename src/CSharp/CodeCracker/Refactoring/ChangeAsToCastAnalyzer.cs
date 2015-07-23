4078fa877687e49142ff4542956e96cef0f74838
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2015-07-23 18:58:51 +0200

    Fix #65 (Change from as operator to direct cast or the opposite)

---
+++ src/CSharp/CodeCracker/Refactoring/ChangeAsToCastAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace CodeCracker.CSharp.Refactoring
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ChangeAsToCastAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string Title = "Toggle between safe cast ('as') and direct cast.";
+        internal const string MessageFormat = "Toggle between safe cast ('as') and direct cast.";
+        internal const string Category = SupportedCategories.Refactoring;
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId.ChangeAsToCast.ToDiagnosticId(),
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Hidden,
+            isEnabledByDefault: true,
+            helpLinkUri: HelpLink.ForDiagnostic(DiagnosticId.ChangeAsToCast));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.AsExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.CastExpression);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            // Exclude cases like "obj as whatever", where "whatever" is not a type
+            var right = (context.Node as BinaryExpressionSyntax)?.Right;
+            if (right != null && !(right is TypeSyntax))
+                return;
+
+            // Exclude casts to value type
+            if (context.Node.Kind() == SyntaxKind.CastExpression)
+            {
+                var type = context.SemanticModel.GetTypeInfo(context.Node).Type;
+                if (type?.IsValueType == true)
+                    return;
+            }
+
+            context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+        }
+    }
+}
---
+++ src/CSharp/CodeCracker/Refactoring/ChangeAsToCastCodeFixProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using System.Linq;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Formatting;
+
+namespace CodeCracker.CSharp.Refactoring
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(ChangeAsToCastCodeFixProvider)), Shared]
+    public class ChangeAsToCastCodeFixProvider : CodeFixProvider
+    {
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DiagnosticId.ChangeAsToCast.ToDiagnosticId());
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var diagnostic = context.Diagnostics.First();
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+            var node = root.FindNode(diagnostic.Location.SourceSpan);
+            if (node.Kind() == SyntaxKind.AsExpression)
+                context.RegisterCodeFix(CodeAction.Create("Change safe cast ('as') to direct cast", ct => ChangeAsToCastAsync(context.Document, root, node)), diagnostic);
+            else if (node.Kind() == SyntaxKind.CastExpression)
+                context.RegisterCodeFix(CodeAction.Create("Change direct cast to safe cast ('as')", ct => ChangeCastToAsAsync(context.Document, root, node)), diagnostic);
+        }
+
+        private static Task<Document> ChangeAsToCastAsync(Document document, SyntaxNode root, SyntaxNode node)
+        {
+            var @as = (BinaryExpressionSyntax) node;
+            var type = (TypeSyntax) @as.Right;
+            var cast = SyntaxFactory.CastExpression(type, @as.Left).WithAdditionalAnnotations(Formatter.Annotation);
+            var newRoot = root.ReplaceNode(@as, cast);
+            return Task.FromResult(document.WithSyntaxRoot(newRoot));
+        }
+
+        private static Task<Document> ChangeCastToAsAsync(Document document, SyntaxNode root, SyntaxNode node)
+        {
+            var cast = (CastExpressionSyntax) node;
+            var @as = SyntaxFactory.BinaryExpression(SyntaxKind.AsExpression, cast.Expression, cast.Type).WithAdditionalAnnotations(Formatter.Annotation);
+            var newRoot = root.ReplaceNode(cast, @as);
+            return Task.FromResult(document.WithSyntaxRoot(newRoot));
+        }
+    }
+}
