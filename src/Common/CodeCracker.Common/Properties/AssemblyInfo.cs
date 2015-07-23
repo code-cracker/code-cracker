2a17fc70e300e3c8fbcaac45b762dad60fc0cc59
Giovanni Bassi <giggio@giggio.net>
2015-07-22 22:23:02 -0300

    update versions to rc1

--- src/Common/CodeCracker.Common/Properties/AssemblyInfo.cs
+++ src/Common/CodeCracker.Common/Properties/AssemblyInfo.cs
@@ -14,6 +14,6 @@ using System.Runtime.CompilerServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.7")]
+[assembly: AssemblyFileVersion("1.0.0.8")]
 [assembly: InternalsVisibleTo("CodeCracker.Test.CSharp")]
 [assembly: InternalsVisibleTo("CodeCracker.Test.VisualBasic")]
