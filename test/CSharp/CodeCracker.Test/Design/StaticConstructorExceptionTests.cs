f5ef8a8e62287efcc7c2fab18f330067325a2ae5
coolrunr@drautage.com <coolrunr@drautage.com>
2015-08-01 17:24:22 -0500

    #352 Add fix all tests for StaticConstructorExceptionCodeFixProvider

--- test/CSharp/CodeCracker.Test/Design/StaticConstructorExceptionTests.cs
+++ test/CSharp/CodeCracker.Test/Design/StaticConstructorExceptionTests.cs
@@ -97,5 +97,43 @@ namespace CodeCracker.Test.CSharp.Design
 
             await VerifyCSharpFixAsync(source, fixtest, 0);
         }
+
+        [Fact]
+        public async Task WhenThrowIsRemovedFromAllStaticConstructors()
+        {
+            const string source1 = @"
+                public class MyClass1 
+                { 
+                    static MyClass1() 
+                    { 
+                        throw new System.Exception(""error message""); 
+                    } 
+                }";
+            const string fixtest1 = @"
+                public class MyClass1 
+                { 
+                    static MyClass1() 
+                    { 
+                    } 
+                }";
+
+            const string source2 = @"
+                public class MyClass2 
+                { 
+                    static MyClass2() 
+                    { 
+                        throw new System.Exception(""error message""); 
+                    } 
+                }";
+            const string fixtest2 = @"
+                public class MyClass2 
+                { 
+                    static MyClass2() 
+                    { 
+                    } 
+                }";
+
+            await VerifyCSharpFixAllAsync(new string[] { source1, source2 }, new string[] { fixtest1, fixtest2 });
+        }
     }
 }
