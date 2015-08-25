257b7d043a27815856527a68fb748881bdf43b40
Þorgeir <thorgeir@radiantgames.is>
2015-08-25 00:36:38 +0000

    Added a new Analyzer (ReturnTaskInsteadOfAwaitAnalyzer)

---
+++ src/CSharp/CodeCracker/Performance/ReturnTaskInsteadOfAwaitAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeCracker.CSharp.Performance
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ReturnTaskInsteadOfAwaitAnalyzer : DiagnosticAnalyzer
+    {
+        internal const string Title = "Remove async and return task directly.";
+        internal const string MessageFormat = "This method can directly return a task.";
+        internal const string Category = SupportedCategories.Performance;
+
+        internal static DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Info,
+            isEnabledByDefault: true,
+            helpLinkUri: HelpLink.ForDiagnostic(DiagnosticId.ReturnTaskInsteadOfAwait));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.MethodDeclaration);
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var methodDecl = (context.Node as MethodDeclarationSyntax);
+            if (!methodDecl.Modifiers.Any(SyntaxKind.AsyncKeyword)) return;
+            if (context.IsGenerated()) return;
+
+            var awaits = from child in methodDecl.Body.DescendantNodes(_ => true)
+                         where child.IsKind(SyntaxKind.AwaitExpression)
+                         let AwaitExpression = child as AwaitExpressionSyntax
+                         select AwaitExpression;
+
+            var blocks = new HashSet<BlockSyntax>();
+            foreach (var await in awaits)
+            {
+                var block = await.FirstAncestorOfType<BlockSyntax>();
+                if (blocks.Contains(block)) return;
+                blocks.Add(block);
+            }
+
+            foreach (var block in blocks)
+            {
+                var lastStatement = block.ChildNodes().Last();
+                if (!lastStatement.IsKind(SyntaxKind.ExpressionStatement)) return;
+                var statement = lastStatement as ExpressionStatementSyntax;
+                if (!statement.Expression.IsKind(SyntaxKind.AwaitExpression)) return;
+            }
+
+            var diagnostic = Diagnostic.Create(Rule, methodDecl.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
---
+++ test/CSharp/CodeCracker.Test/Performance/ReturnTaskInsteadOfAwaitTests.cs
@@ -0,0 +1,38 @@
+﻿using CodeCracker.CSharp.Performance;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeCracker.Test.CSharp.Performance
+{
+    public class ReturnTaskInsteadOfAwaitTests : DiagnosticVerifier<ReturnTaskInsteadOfAwaitAnalyzer>
+    {
+        [Fact]
+        public async Task ReturnTaskDirectlyWithSimpleFunction()
+        {
+            const string test = @"
+                async void FooAsync()
+                {
+                    Console.WriteLine(42);
+                    await SomethingElseAsync();
+                }
+                
+                async Task SomethingElseAsync()
+                {
+                    await Task.Delay(200);
+                    Console.WriteLine(""Done Waiting"");
+                }
+                ";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+                Message = "This method can directly return a task.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+    }
+}
