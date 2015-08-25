e638cdaae22f092cfab419fadaf55c2ddf90f7b9
Þorgeir <thorgeir@radiantgames.is>
2015-08-25 02:35:27 +0000

    Added more Tests for the ReturnTaskInsteadOfAwait

--- test/CSharp/CodeCracker.Test/Performance/ReturnTaskInsteadOfAwaitTests.cs
+++ test/CSharp/CodeCracker.Test/Performance/ReturnTaskInsteadOfAwaitTests.cs
@@ -7,20 +7,83 @@ namespace CodeCracker.Test.CSharp.Performance
 {
     public class ReturnTaskInsteadOfAwaitTests : DiagnosticVerifier<ReturnTaskInsteadOfAwaitAnalyzer>
     {
+
+        [Fact]
+        public async Task IgnoresWhenWorkIsDoneAfterTheAwait()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(42);
+                    await Task.Delay(200);
+                    Console.WriteLine(""Done Waiting"");
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenWorkIsDone()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(42);
+                    await Task.Delay(200);
+                    Console.WriteLine(""Done Waiting"");
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenMultipeAwaits()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(42);
+                    await Task.Delay(200);
+                    Console.WriteLine(""Done Waiting"");
+                    await Task.Delay(200);
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenMultipeAwaitsInsideABlock()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    if (true)
+                    {
+                        Console.WriteLine(42);
+                        await Task.Delay(200);
+
+                        Console.WriteLine(""foo"");
+                        await Task.Delay(200);
+                    }
+                    else
+                    {
+                        Console.WriteLine(""foo"");
+                        await Task.Delay(200);
+                    }
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+
         [Fact]
         public async Task ReturnTaskDirectlyWithSimpleFunction()
         {
             const string test = @"
-                async void FooAsync()
+                async Task FooAsync()
                 {
                     Console.WriteLine(42);
-                    await SomethingElseAsync();
-                }
-                
-                async Task SomethingElseAsync()
-                {
                     await Task.Delay(200);
-                    Console.WriteLine(""Done Waiting"");
                 }
                 ";
 
@@ -34,5 +97,37 @@ namespace CodeCracker.Test.CSharp.Performance
 
             await VerifyCSharpDiagnosticAsync(test, expected);
         }
+
+        [Fact]
+        public async Task ReturnTaskDirectlyWithMultipleBranches()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    if (true)
+                    {
+                        Console.WriteLine(42);
+                        await Task.Delay(200);
+                    }
+                    else
+                    {
+                        Console.WriteLine(""foo"");
+                        await Task.Delay(200);
+                    }
+                }
+                ";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+                Message = "This method can directly return a task.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+
     }
 }
