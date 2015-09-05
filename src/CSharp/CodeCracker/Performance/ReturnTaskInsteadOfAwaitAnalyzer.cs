171bc1fa2b2a14fd1d12f2f6cf4133395625690d
Þorgeir <thorgeir@radiantgames.is>
2015-09-05 16:48:38 +0000

    ReturnTaskInsteadOfAwaitTests now cabable of detecting nested if and switch branching. Added 20 unit test to cover most cases.

--- src/CSharp/CodeCracker/Performance/ReturnTaskInsteadOfAwaitAnalyzer.cs
+++ src/CSharp/CodeCracker/Performance/ReturnTaskInsteadOfAwaitAnalyzer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis;
+﻿
+
+
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -6,6 +9,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading.Tasks;
+using System;
 
 namespace CodeCracker.CSharp.Performance
 {
@@ -34,38 +38,44 @@ namespace CodeCracker.CSharp.Performance
             var methodDecl = (context.Node as MethodDeclarationSyntax);
             if (!methodDecl.Modifiers.Any(SyntaxKind.AsyncKeyword)) return;
             if (context.IsGenerated()) return;
-            if (methodDecl.ExpressionBody != null) return; // TODO: Workout for expressionBody
+            if (methodDecl.ExpressionBody != null) return;
 
-
-            var awaits = from child in methodDecl.Body.DescendantNodes(_ => true)
-                         where child.IsKind(SyntaxKind.AwaitExpression)
-                         let AwaitExpression = child as AwaitExpressionSyntax
-                         select AwaitExpression;
-
-            foreach (var await in awaits)
+            var awaits = new List<StatementSyntax>();
+            var AnyReturns = false;
+            foreach (var child in methodDecl.Body.DescendantNodes(_ => true))
             {
-                var ansestors = await.Ancestors();
-                if (ansestors.Any(ansestor => IsLoopStatement(ansestor))) return;
+                if (child.IsKind(SyntaxKind.AwaitExpression))
+                {
+                    if (child.Ancestors().Any(ansestor => IsLoopStatement(ansestor))) return;
+                    var awaitStatement = child.FirstAncestorOrSelfThatIsAStatement();
+                    awaits.Add(awaitStatement);
+                }
+                else if (child.IsKind(SyntaxKind.ReturnStatement))
+                    AnyReturns = true;
             }
+            if (awaits.Count == 0) return;
 
-            var controlFlow = context.SemanticModel.AnalyzeControlFlow(methodDecl.Body);
-            if (controlFlow.ReturnStatements.Length == 0)
+            var returnType = context.SemanticModel.GetTypeInfo(methodDecl.ReturnType);
+
+            if (returnType.Type.SpecialType == SpecialType.System_Void ||
+                returnType.Type.MetadataName.Equals(nameof(Task)))
             {
-                //var returnType = context.SemanticModel.GetTypeInfo(methodDecl.ReturnType);
-                //returnType.ConvertedType.spec;
-
-                var lastStatement = methodDecl.Body.ChildNodes().Last();
-                if (!lastStatement.IsKind(SyntaxKind.ExpressionStatement)) return;
-                var statement = lastStatement as ExpressionStatementSyntax;
-                if (!statement.Expression.IsKind(SyntaxKind.AwaitExpression)) return;
-
-
+                if (AnyReturns) return;
+                var CheckedBranches = new Dictionary<StatementSyntax, bool>();
+                foreach (var awaitStatement in awaits)
+                {
+                    var lastSiblingOfAwaitParent = SiblingsAndSelf(awaitStatement)
+                        .Last(node => !IsExpressionLessStatement(node));
+                    if (lastSiblingOfAwaitParent != awaitStatement) return;
+                    if (!CheckBranching(awaitStatement, CheckedBranches)) return;
+                }
+                if (CheckedBranches.ContainsValue(false)) return;
             }
             else
             {
-                foreach (var await in awaits)
+                foreach (var awaitStatements in awaits)
                 {
-                    if (!await.Parent.IsKind(SyntaxKind.ReturnStatement)) return;
+                    if (!awaitStatements.IsKind(SyntaxKind.ReturnStatement)) return;
                 }
             }
 
@@ -73,16 +83,90 @@ namespace CodeCracker.CSharp.Performance
             context.ReportDiagnostic(diagnostic);
         }
 
-        private static bool IsLoopStatement(SyntaxNode note) => note.IsAnyKind(SyntaxKind.ForEachStatement, SyntaxKind.ForStatement, SyntaxKind.WhileStatement);
 
-
-        async static Task FooAsync(bool x)
+        /// <summary>
+        /// Checks if the branch contains an await in all as the last statement. Returns true if the checking should continue.
+        /// </summary>
+        /// <param name="statement">Statement to look at</param>
+        /// <param name="checkedBranches">Lookup for the branches already checked.</param>
+        /// <returns>True if the checking should continue.</returns>
+        private static bool CheckBranching(StatementSyntax statement, Dictionary<StatementSyntax, bool> checkedBranches)
         {
-            System.Math.Abs(12);
-            if (x)
-                await Task.Delay(200);
-            else
-                await Task.Delay(200);
+            if (statement == null) return true;
+            if (checkedBranches.ContainsKey(statement) && checkedBranches[statement]) return true;
+            checkedBranches[statement] = false;
+
+            if (statement.IsKind(SyntaxKind.IfStatement))
+            {
+                var IfStatement = statement as IfStatementSyntax;
+                var ifHasAwait = BranchContainsAwait(IfStatement.Statement, true, checkedBranches);
+                var elseHasAwait = BranchContainsAwait(IfStatement.Else?.Statement, false, checkedBranches);
+                if (!ifHasAwait || !elseHasAwait) return true;
+
+                var lastSibling = SiblingsAndSelf(IfStatement).Last(statmement => !IsExpressionLessStatement(statmement));
+                if (lastSibling != IfStatement) return true;
+            }
+            else if (statement.IsKind(SyntaxKind.SwitchStatement))
+            {
+                var SwitchStatement = statement as SwitchStatementSyntax;
+                var containsDefault = false;
+                foreach (var section in SwitchStatement.Sections)
+                {
+                    if (section.Labels.Any(SyntaxKind.DefaultSwitchLabel)) containsDefault = true;
+                    var lastStatementInSection = section.Statements.Last(statmement => !IsExpressionLessStatement(statmement));
+                    var hasAwait = BranchContainsAwait(lastStatementInSection, false, checkedBranches);
+                    if (!hasAwait) return true;
+                }
+                if (!containsDefault) return false;
+            }
+            checkedBranches[statement] = true;
+            return CheckBranching(statement.Parent.FirstAncestorOrSelfThatIsAStatement(), checkedBranches);
         }
+
+        private static bool BranchContainsAwait(StatementSyntax statement, bool nestedStatments, Dictionary<StatementSyntax, bool> checkedBranches)
+        {
+            if (statement == null) return false;
+            if (checkedBranches.ContainsKey(statement) && checkedBranches[statement]) return true;
+
+            var singleStatement = statement.GetSingleStatementFromPossibleBlock();
+            if (singleStatement != null && singleStatement != statement)
+            {
+                if (checkedBranches.ContainsKey(singleStatement) && checkedBranches[singleStatement]) return true;
+                return GetAwaitExpression(singleStatement) != null;
+            }
+
+            if (nestedStatments)
+            {
+                var statements = statement.ChildNodes();
+                var lastStatement = statements.Last(statmement => !IsExpressionLessStatement(statmement));
+                return GetAwaitExpression(lastStatement) != null;
+            }
+            else
+                return GetAwaitExpression(statement) != null;
+        }
+
+        private static AwaitExpressionSyntax GetAwaitExpression(SyntaxNode syntaxNode)
+        {
+            if (syntaxNode.IsKind(SyntaxKind.ExpressionStatement))
+            {
+                var statementExpr = (syntaxNode as ExpressionStatementSyntax);
+                if (statementExpr.Expression.IsKind(SyntaxKind.AwaitExpression))
+                {
+                    return statementExpr.Expression as AwaitExpressionSyntax;
+                }
+            }
+            else if (syntaxNode.IsKind(SyntaxKind.AwaitExpression))
+            {
+                return syntaxNode as AwaitExpressionSyntax;
+            }
+            return null;
+        }
+
+        private static IEnumerable<SyntaxNode> SiblingsAndSelf(SyntaxNode retStatment) => retStatment.Parent.ChildNodes();
+
+        private static bool IsReturnStatement(SyntaxNode note) => note.IsKind(SyntaxKind.ReturnStatement);
+        private static bool IsLoopStatement(SyntaxNode note) => note.IsAnyKind(SyntaxKind.ForEachStatement, SyntaxKind.ForStatement, SyntaxKind.WhileStatement, SyntaxKind.DoStatement);
+        private static bool IsExpressionLessStatement(SyntaxNode node) => node.IsKind(SyntaxKind.ReturnStatement, SyntaxKind.ElseClause, SyntaxKind.BreakStatement);
+
     }
 }
--- test/CSharp/CodeCracker.Test/Performance/ReturnTaskInsteadOfAwaitTests.cs
+++ test/CSharp/CodeCracker.Test/Performance/ReturnTaskInsteadOfAwaitTests.cs
@@ -60,7 +60,7 @@ namespace CodeCracker.Test.CSharp.Performance
             await VerifyCSharpHasNoDiagnosticsAsync(test);
         }
         [Fact]
-        public async Task IgnoresGenericTasksWhenWorkIsDoneAfterAwait()
+        public async Task IgnoreGenericTasksWhenWorkIsDoneAfterAwait()
         {
             const string test = @"
                 async Task<int> FooAsync()
@@ -73,7 +73,7 @@ namespace CodeCracker.Test.CSharp.Performance
             await VerifyCSharpHasNoDiagnosticsAsync(test);
         }
         [Fact]
-        public async Task IgnoresWhenMultipeAwaits()
+        public async Task IgnoreMultipeAwaits()
         {
             const string test = @"
                 async Task FooAsync()
@@ -87,7 +87,7 @@ namespace CodeCracker.Test.CSharp.Performance
             await VerifyCSharpHasNoDiagnosticsAsync(test);
         }
         [Fact]
-        public async Task IgnoresWhenMultipeAwaitsInsideABlock()
+        public async Task IgnoreWhenMultipeAwaitsInsideABlock()
         {
             const string test = @"
                 async Task FooAsync()
@@ -105,6 +105,114 @@ namespace CodeCracker.Test.CSharp.Performance
                         Console.WriteLine(""foo"");
                         await Task.Delay(200);
                     }
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+        [Fact]
+        public async Task IgnoreSwitchSectionWithoutAwait()
+        {
+            const string test = @"
+                async Task FooAsync(int x)
+                {
+                    Console.WriteLine(""foo"");
+                    switch (x)
+                    {
+                        case 1:
+                            await Task.Delay(10);
+                            break;
+                        case 2:
+                            Console.WriteLine(""foo"");
+                            break;
+                        default:
+                            await Task.Delay(42);
+                            break;
+                    }
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+        [Fact]
+        public async Task IgnoreSwitchWithOutDefault()
+        {
+            const string test = @"
+                async Task FooAsync(int x)
+                {
+                    Console.WriteLine(""foo"");
+                    switch (x)
+                    {
+                        case 1:
+                            await Task.Delay(10);
+                            break;
+                        case 2:
+                            await Task.Delay(42);
+                            break;
+                    }
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task IgnoresWhenAwaitIsAfterBlock()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    if (true)
+                    {
+                        Console.WriteLine(42);
+                        await Task.Delay(200);
+                    }
+                    else
+                    {
+                        Console.WriteLine(""foo"");
+                        await Task.Delay(200);
+                    }
+                    await Task.Delay(200);
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+        [Fact]
+        public async Task IgnoreAwaitsAfterBranching()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(""foo"");
+                    if (true)
+                       await Task.Delay(200);
+                    else
+                        Console.WriteLine(""42"");
+                    
+                    await Task.Delay(200);
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+        [Fact]
+        public async Task IgnoreAwaitsOnlyInSingleBranch()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(""foo"");
+                    if (true)
+                       await Task.Delay(200);
+                }
+                ";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+        [Fact]
+        public async Task IgnoreReturnWithVoid()
+        {
+            const string test = @"
+                async void FooAsync()
+                {
+                    Console.WriteLine(42);rv 
+                    await Task.Delay(200);
+                    return;
                 }
                 ";
             await VerifyCSharpHasNoDiagnosticsAsync(test);
@@ -148,6 +256,152 @@ namespace CodeCracker.Test.CSharp.Performance
                     }
                 }
                 ";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+                Message = "This method can directly return a task.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+        [Fact]
+        public async Task ReturnTaskDirectlyWithDeepBranching()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(""foo"");
+                    if (true)
+                        if (true)
+                            await Task.Delay(200);
+                        else
+                           await Task.Delay(200);
+                    else
+                        if (true)
+                            await Task.Delay(200);
+                        else
+                           await Task.Delay(200);
+                }
+                ";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+                Message = "This method can directly return a task.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+        [Fact]
+        public async Task ReturnTaskDirectlyWithDeepBranchingAndMoreStatements()
+        {
+            const string test = @"
+                async Task FooAsync()
+                {
+                    Console.WriteLine(""foo"");
+                    if (true)
+                        if (true)
+                            await Task.Delay(10);
+                        else
+                           await Task.Delay(20);
+                    else
+                    {
+                        if (true)
+                        {
+                            Console.WriteLine(""foo"");
+                            await Task.Delay(30);
+                        }
+                        else
+                           await Task.Delay(40);
+                    }
+                }
+                ";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+                Message = "This method can directly return a task.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task ReturnTaskDirectlyWithSwitchBranching()
+        {
+            const string test = @"
+                async Task FooAsync(int x)
+                {
+                    Console.WriteLine(""foo"");
+                    switch (x)
+                    {
+                        case 1:
+                            await Task.Delay(10);
+                            break;
+                        case 2:
+                            await Task.Delay(20);
+                            break;
+                        case 3:
+                            await Task.Delay(30);
+                            break;
+                        default:
+                            await Task.Delay(42);
+                            break;
+                    }
+                }
+                ";
+
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
+                Message = "This method can directly return a task.",
+                Severity = DiagnosticSeverity.Info,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+        [Fact]
+        public async Task ReturnTaskDirectlyWithBothSwitchIfBranching()
+        {
+            const string test = @"
+                async Task FooAsync(int x)
+                {
+                    Console.WriteLine(""foo"");                       
+                    if (true)
+                        await Task.Delay(200);
+                    else
+                    {
+                        switch (x)
+                        {
+                            case 1:
+                                if (true)
+                                    await Task.Delay(200);
+                                else
+                                    await Task.Delay(200);
+                                break;
+                            case 2:
+                                await Task.Delay(20);
+                                break;
+                            case 3:
+                                await Task.Delay(30);
+                                break;
+                            default:
+                                await Task.Delay(42);
+                                break;
+                        }
+                    }
+                }
+                ";
+
 
             var expected = new DiagnosticResult
             {
@@ -221,27 +475,6 @@ namespace CodeCracker.Test.CSharp.Performance
                 }
 
                 async Task<int> Sum(int x, int y) { await Task.Delay(200); return x + y; }
-                ";
-            var expected = new DiagnosticResult
-            {
-                Id = DiagnosticId.ReturnTaskInsteadOfAwait.ToDiagnosticId(),
-                Message = "This method can directly return a task.",
-                Severity = DiagnosticSeverity.Info,
-                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 2, 17) }
-            };
-
-            await VerifyCSharpDiagnosticAsync(test, expected);
-        }
-        [Fact]
-        public async Task VoidReturnWithAwaits()
-        {
-            const string test = @"
-                async Task FooAsync()
-                {
-                    Console.WriteLine(42);
-                    await Task.Delay(200);
-                    return;
-                }
                 ";
             var expected = new DiagnosticResult
             {
