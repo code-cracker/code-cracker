5ba886c24044112766f61d2f60a516f0b2540ac2
Leo Shine <shine.leo@gmail.com>
2015-10-15 08:29:15 +0100

    Convert method to property

--- src/Common/CodeCracker.Common/DiagnosticId.cs
+++ src/Common/CodeCracker.Common/DiagnosticId.cs
@@ -85,6 +85,7 @@
         ExtractClassToFile = 96,
         ChangeCountMethodToProperty = 98,
         PreferAnyToCountGreaterThanZero = 99,
+        ConvertMethodToProperty = 103,
         AlwaysUseVarOnPrimitives = 105,
         NameOf_External = 108,
         InconsistentParameterDefaultValue = 110,
