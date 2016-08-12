9fea23da73b52f19ec12a8eae306802ac015c714
Amadeusz Wieczorek <git@amadeusw.com>
2016-08-12 09:02:46 -0700

    CC0122 Replace Task.Result with await Task

---
+++ src/CSharp/CodeCracker/Design/ResultInAsyncAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using CodeCracker.Properties;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace CodeCracker.CSharp.Design
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ResultInAsyncAnalyzer : DiagnosticAnalyzer
+    {
+        internal static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.ResultInAsyncAnalyzer_Title), Resources.ResourceManager, typeof(Resources));
+        internal static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.ResultInAsync_MessageFormat), Resources.ResourceManager, typeof(Resources));
+        internal static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.ResultInAsync_Description), Resources.ResourceManager, typeof(Resources));
+        internal const string Category = SupportedCategories.Design;
+
+        internal static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
+            DiagnosticId.ResultInAsync.ToDiagnosticId(),
+            Title,
+            MessageFormat,
+            Category,
+            DiagnosticSeverity.Warning,
+            true,
+            description: Description,
+            helpLinkUri: HelpLink.ForDiagnostic(DiagnosticId.ResultInAsync));
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context) =>
+            context.RegisterSyntaxNodeAction(Analyzer, SyntaxKind.InvocationExpression);
+
+        private static void Analyzer(SyntaxNodeAnalysisContext context)
+        {
+            if (context.IsGenerated()) return;
+            var invocation = (InvocationExpressionSyntax)context.Node;
+            var parentMethod = invocation.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
+            if (parentMethod == null) return;
+            var parentIsAsync = parentMethod.Modifiers.Any(n => n.IsKind(SyntaxKind.AsyncKeyword));
+            if (!parentIsAsync) return;
+            // We now know that we are in async method
+
+            var memberAccess = invocation.Parent as MemberAccessExpressionSyntax;
+            if (memberAccess == null) return;
+            var member = memberAccess.Name;
+            if (member.ToString() != "Result") return;
+            // We now know that we are accessing .Result
+
+            var identifierSymbol = context.SemanticModel.GetSymbolInfo(memberAccess, context.CancellationToken).Symbol;
+            if (identifierSymbol.OriginalDefinition.ToString() != "System.Threading.Tasks.Task<TResult>.Result") return;
+            // We now know that we are accessing System.Threading.Tasks.Task<TResult>.Result
+
+            SimpleNameSyntax identifier;
+            identifier = invocation.Expression as IdentifierNameSyntax;
+            if (identifier == null)
+            {
+                var transient = invocation.Expression as MemberAccessExpressionSyntax;
+                identifier = transient.Name;
+            }
+            if (identifier == null) return; // It's not supposed to happen. Don't throw an exception, though.
+            context.ReportDiagnostic(Diagnostic.Create(Rule, identifier.GetLocation(), identifier.Identifier.Text));
+        }
+    }
+}
---
+++ test/CSharp/CodeCracker.Test/Design/ResultInAsyncTest.cs
@@ -0,0 +1,167 @@
+﻿using CodeCracker.CSharp.Design;
+using Microsoft.CodeAnalysis;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CodeCracker.Test.CSharp.Design
+{
+    public class ResultInAsyncTest : CodeFixVerifier<ResultInAsyncAnalyzer, ResultInAsyncCodeFixProvider>
+    {
+        [Fact]
+        public async Task ResultInNonAsyncMethodIsOk()
+        {
+            const string test = @"
+                using System.Threading.Tasks;
+                public class MyClass
+                {
+                    public Task Execute()
+                    {
+                        return Asynchronous().Result;
+                    }
+                    public async Task Asynchronous()
+                    {
+                        return;
+                    }
+                }";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task WarningIfResultInAsync()
+        {
+            const string test = @"
+                using System.Threading.Tasks;
+                public class MyClass
+                {
+                    public async Task<int> Execute()
+                    {
+                        return Asynchronous().Result;
+                    }
+                    public async Task<int> Asynchronous()
+                    {
+                        return 5;
+                    }
+                }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ResultInAsync.ToDiagnosticId(),
+                Message = string.Format(ResultInAsyncAnalyzer.MessageFormat.ToString(), "Asynchronous"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 7, 32) }
+            };
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task FixResultInAsync()
+        {
+            const string source = @"
+                using System.Threading.Tasks;
+                public class MyClass
+                {
+                    public async Task<int> Execute()
+                    {
+                        return Asynchronous().Result;
+                    }
+                    public async Task<int> Asynchronous()
+                    {
+                        return 5;
+                    }
+                }";
+            const string fixtest = @"
+                using System.Threading.Tasks;
+                public class MyClass
+                {
+                    public async Task<int> Execute()
+                    {
+                        return await Asynchronous();
+                    }
+                    public async Task<int> Asynchronous()
+                    {
+                        return 5;
+                    }
+                }";
+            await VerifyCSharpFixAsync(source, fixtest);
+        }
+
+        [Fact]
+        public async Task WarningIfNestedResultInAsync()
+        {
+            const string test = @"
+        namespace Nested.Namespaces
+        {
+            using System.Threading.Tasks;
+            public class ParentClass
+            {
+                public class MyClass
+                {
+                    public async Task Execute()
+                    {
+                        var x = ParentClass.MyClass.Asynchronous().Result.Length;
+                    }
+                    public static async Task<string> Asynchronous()
+                    {
+                        return ""Test"";
+                    }
+                }
+            }
+        }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.ResultInAsync.ToDiagnosticId(),
+                Message = string.Format(ResultInAsyncAnalyzer.MessageFormat.ToString(), "Asynchronous"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 11, 53) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task FixNestedResultInAsync()
+        {
+            const string source = @"
+        namespace Nested.Namespaces
+        {
+            using System.Threading.Tasks;
+            public class ParentClass
+            {
+                public class MyClass
+                {
+                    public async Task Execute()
+                    {
+                        var x = MyClass.Asynchronous().Result.Length;
+                    }
+                    public static async Task<string> Asynchronous()
+                    {
+                        return ""Test"";
+                    }
+                }
+            }
+        }";
+            const string fixtest = @"
+        namespace Nested.Namespaces
+        {
+            using System.Threading.Tasks;
+            public class ParentClass
+            {
+                public class MyClass
+                {
+                    public async Task Execute()
+                    {
+                        var x = (await MyClass.Asynchronous()).Length;
+                    }
+                    public static async Task<string> Asynchronous()
+                    {
+                        return ""Test"";
+                    }
+                }
+            }
+        }";
+
+            await VerifyCSharpFixAsync(source, fixtest);
+        }
+    }
+}
