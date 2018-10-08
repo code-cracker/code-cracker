a6c1d1b93a81efd3da2dfce464963e127a26f16d
carlilord <weilguny.carl@gmx.at>
2018-10-08 10:28:24 +0200

    Make CC0011 work with IQueryable and Async methods
    Fixes #905

--- src/CSharp/CodeCracker/Performance/RemoveWhereWhenItIsPossibleAnalyzer.cs
+++ src/CSharp/CodeCracker/Performance/RemoveWhereWhenItIsPossibleAnalyzer.cs
@@ -25,8 +25,17 @@ namespace CodeCracker.CSharp.Performance
             "Any",
             "Single",
             "SingleOrDefault",
-            "Count"
+            "Count",
+            "FirstAsync",
+            "FirstOrDefaultAsync",
+            "LastAsync",
+            "LastOrDefaultAsync",
+            "AnyAsync",
+            "SingleAsync",
+            "SingleOrDefaultAsync",
+            "CountAsync"
         };
+
         internal static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(
             DiagnosticId.RemoveWhereWhenItIsPossible.ToDiagnosticId(),
             Title,
