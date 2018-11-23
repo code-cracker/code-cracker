80f53b29811fdf36a57a8b8119eea8fc33f25456
carlilord <weilguny.carl@gmx.at>
2018-11-23 10:08:54 +0100

    #905 added unit test

--- test/CSharp/CodeCracker.Test/Performance/RemoveWhereWhenItIsPossibleTests.cs
+++ test/CSharp/CodeCracker.Test/Performance/RemoveWhereWhenItIsPossibleTests.cs
@@ -169,5 +169,165 @@ namespace Sample
             await VerifyCSharpFixAsync(test, expected);
 
         }
+
+        // Async
+        [Theory]
+        [InlineData("FirstAsync")]
+        [InlineData("FirstOrDefaultAsync")]
+        [InlineData("LastAsync")]
+        [InlineData("LastOrDefaultAsync")]
+        [InlineData("AnyAsync")]
+        [InlineData("SingleAsync")]
+        [InlineData("SingleOrDefaultAsync")]
+        [InlineData("CountAsync")]
+        public async Task CreateDiagnosticWhenUsingWhereWithAsync(string method)
+        {
+            var test = @"
+using System.Linq;
+
+namespace Sample
+{
+    public class Foo
+    {
+        public async Task DoSomething()
+        {
+            var a = new int[10];
+            var f = a.Where(item => item > 10)." + method + @"();
+        }
+    }
+}";
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticId.RemoveWhereWhenItIsPossible.ToDiagnosticId(),
+                Message = "You can remove 'Where' moving the predicate to '" + method + "'.",
+                Severity = DiagnosticSeverity.Warning,
+                Locations = new[] { new DiagnosticResultLocation("Test0.cs", 11, 23) }
+            };
+
+            await VerifyCSharpDiagnosticAsync(test, expected);
+
+        }
+
+        [Theory]
+        [InlineData("FirstAsync")]
+        [InlineData("FirstOrDefaultAsync")]
+        [InlineData("LastAsync")]
+        [InlineData("LastOrDefaultAsync")]
+        [InlineData("AnyAsync")]
+        [InlineData("SingleAsync")]
+        [InlineData("SingleOrDefaultAsync")]
+        [InlineData("CountAsync")]
+        public async Task DoNotCreateDiagnosticWhenUsingWhereAndAnotherMethodWithPredicatesAsync(string method)
+        {
+            var test = @"
+using System.Linq;
+
+namespace Sample
+{
+    public class Foo
+    {
+        public async Task DoSomething()
+        {
+            var a = new int[10];
+            var f = a.Where(item => item > 10)." + method + @"(item => item < 50);
+        }
+    }
+}";
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Fact]
+        public async Task DoNotCreateDiagnosticWhenWhereUsesIndexerAsync()
+        {
+            var test = @"
+var first = Enumerable.Range(1, 10).ToList();
+var second = Enumerable.Range(1, 10);
+var isNotMatch = second.Where((t, i) => first[i] != t).Any();
+".WrapInCSharpMethod(usings: "using System.Linq;");
+            await VerifyCSharpHasNoDiagnosticsAsync(test);
+        }
+
+        [Theory]
+        [InlineData("FirstAsync")]
+        [InlineData("FirstOrDefaultAsync")]
+        [InlineData("LastAsync")]
+        [InlineData("LastOrDefaultAsync")]
+        [InlineData("AnyAsync")]
+        [InlineData("SingleAsync")]
+        [InlineData("SingleOrDefaultAsync")]
+        [InlineData("CountAsync")]
+        public async Task FixRemovesWhereMovingPredicateToAsync(string method)
+        {
+            var test = @"
+namespace Sample
+{
+    public class Foo
+    {
+        public async Task DoSomething()
+        {
+            var a = new int[10];
+            var f = a.Where((item) => item > 10)." + method + @"();
+        }
+    }
+}";
+            var expected = @"
+namespace Sample
+{
+    public class Foo
+    {
+        public async Task DoSomething()
+        {
+            var a = new int[10];
+            var f = a." + method + @"((item) => item > 10);
+        }
+    }
+}";
+            await VerifyCSharpFixAsync(test, expected);
+        }
+
+        [Theory]
+        [InlineData("FirstAsync")]
+        [InlineData("FirstOrDefaultAsync")]
+        [InlineData("LastAsync")]
+        [InlineData("LastOrDefaultAsync")]
+        [InlineData("AnyAsync")]
+        [InlineData("SingleAsync")]
+        [InlineData("SingleOrDefaultAsync")]
+        [InlineData("CountAsync")]
+        public async Task FixRemovesWherePreservingPreviousExpressionsMovingPredicateToAsync(string method)
+        {
+            var test = @"
+using System.Linq;
+
+namespace Sample
+{
+    public class Foo
+    {
+        public async Task DoSomething()
+        {
+            var a = new int[10];
+            var f = a.OrderBy(item => item).Where(item => item > 10)." + method + @"();
+        }
+    }
+}";
+
+            var expected = @"
+using System.Linq;
+
+namespace Sample
+{
+    public class Foo
+    {
+        public async Task DoSomething()
+        {
+            var a = new int[10];
+            var f = a.OrderBy(item => item)." + method + @"(item => item > 10);
+        }
+    }
+}";
+
+            await VerifyCSharpFixAsync(test, expected);
+
+        }
     }
 }
