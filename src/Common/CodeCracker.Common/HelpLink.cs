47d978db48159535e70a4f41d2af288cf8f82936
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-10 21:18:46 +0800

    Delete src directory

--- src/Common/CodeCracker.Common/HelpLink.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodeCracker
-{
-    public static class HelpLink
-    {
-        public static string ForDiagnostic(DiagnosticId diagnosticId) =>
-            $"https://code-cracker.github.io/diagnostics/{diagnosticId.ToDiagnosticId()}.html";
-    }
-}
